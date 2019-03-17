cf8f803ebdc4b03af4a1a94b0335639f4c2c58b4
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-03-16 21:33:31 -0700

    DSG: Design Tic-Tac-Toe

---
+++ DSG: Design Tic-Tac-Toe.java
@@ -0,0 +1,63 @@
+/*
+https://leetcode.com/problems/design-tic-tac-toe/
+Design a Tic-tac-toe game that is played between two players on a n x n grid.
+
+You may assume the following rules:
+
+A move is guaranteed to be valid and is placed on an empty block.
+Once a winning condition is reached, no more moves is allowed.
+A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
+
+we don't need to keep track of an entire n^2 board. We only need to keep a count for each row and column. 
+If at any time a row or column matches the size of the board then that player has won.
+*/
+class TicTacToe {
+    private int[] rows;
+    private int[] cols;
+    private int diagonal;
+    private int antiDiagonal;
+    int size = 0;
+    /** Initialize your data structure here. */
+    public TicTacToe(int n) {
+        rows = new int[n];
+        cols = new int[n];
+        size =n;
+    }
+    
+    /** Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins. */
+    public int move(int row, int col, int player) {
+        int toAdd = player == 1 ? 1 : -1;
+    
+        rows[row] += toAdd;
+        cols[col] += toAdd;
+        if (row == col){
+            diagonal += toAdd;
+        }
+
+        if (row+col == size-1){
+            antiDiagonal += toAdd;
+        }
+
+        if (Math.abs(rows[row]) == size ||
+            Math.abs(cols[col]) == size ||
+            Math.abs(diagonal) == size  ||
+            Math.abs(antiDiagonal) == size){
+            return player;
+        }
+
+        return 0;
+    }
+}
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe obj = new TicTacToe(n);
+ * int param_1 = obj.move(row,col,player);
+ */
