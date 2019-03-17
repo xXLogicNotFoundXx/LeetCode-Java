ccb28aa72c757b8a73323f34bc4ae62dca852af9
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-03-16 18:28:25 -0700

    Array - Median finder from Data Stream

---
+++ Array - Median finder from Data Stream.java
@@ -0,0 +1,36 @@
+/*
+https://leetcode.com/problems/find-median-from-data-stream/solution/
+Median is the middle value in an ordered integer list. 
+If the size of the list is even, there is no middle value. 
+So the median is the mean of the two middle value.
+
+For example,
+[1] =>  1
+[2,3], the median is (2 + 3) / 2 = 2.5
+[2,3,4] => 3
+
+*/
+
+class MedianFinder {
+    PriorityQueue<Integer> leftMaxHeap = new PriorityQueue<Integer>(Collections.reverseOrder()); 
+    PriorityQueue<Integer> rightMinHeap =  new PriorityQueue<Integer>(); 
+    /** initialize your data structure here. */
+    public MedianFinder() {
+    }
+   
+    public void addNum(int num) {
+        leftMaxHeap.offer(num);
+        rightMinHeap.offer(leftMaxHeap.poll());
+        if (leftMaxHeap.size() < rightMinHeap.size()){
+            leftMaxHeap.offer(rightMinHeap.poll());
+        }
+    }
+
+    // Returns the median of current data stream
+    public double findMedian() {
+        if (leftMaxHeap.size() == rightMinHeap.size()) 
+            return leftMaxHeap.peek()/2.0 + rightMinHeap.peek()/2.0;
+        else return leftMaxHeap.peek();
+    }
+    
+}
