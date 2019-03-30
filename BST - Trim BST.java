f1fabf5f7839c50f944dc17d2963c366bd725aae
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-03-29 22:57:47 -0700

    BST - Trim BST  : remove nodes outside of range L and R values

---
+++ BST - Trim BST.java
@@ -0,0 +1,59 @@
+/*
+https://leetcode.com/problems/trim-a-binary-search-tree/
+Given a binary search tree and the lowest and highest boundaries as L and R, trim the tree so that all its elements lies in [L, R] (R >= L). 
+Input: 
+    3
+   / \
+  0   4
+   \
+    2
+   /
+  1
+L = 1  R = 3
+Output: 
+      3
+     / 
+   2 
+
+
+L=32 R=44
+  45
+ /  \
+30  46
+ \
+  36
+  
+o/p: 36
+
+*/
+class Solution {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        
+        // Find a valid root which is used to return. For following recursive function this code block is not necessary 
+        // this will be handled automatically in helper function. 
+        while (root!=null && (root.val < L || root.val > R)) {
+            if (root.val < L) 
+                root = root.right;
+            
+            if (root.val > R) 
+                root = root.left;
+        }
+
+        return trimBSTHelper(root,L,R);
+    }
+    
+    public TreeNode trimBSTHelper(TreeNode root, int L, int R) {
+        if (root == null) 
+            return null;
+        
+        if (root.val < L) 
+            return trimBSTHelper(root.right, L, R);
+        if (root.val > R) 
+            return trimBSTHelper(root.left, L, R);
+        
+        root.left = trimBSTHelper(root.left, L, R);
+        root.right = trimBSTHelper(root.right, L, R);
+        
+        return root;
+    }
+}
