ac12c3f1965e9a4918de5358ec41b693d721304b
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-04-01 23:26:54 -0700

    LL - Simple.java

--- LL - Simple.java
+++ LL - Simple.java
@@ -12,3 +12,26 @@ class Solution {
         dummy.next =null;
     }
 }
+
+24. Swap Nodes in Pairs
+https://leetcode.com/problems/swap-nodes-in-pairs/submissions/
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode start = dummy;
+        while(dummy.next != null && dummy.next.next!=null){
+            swap(dummy);
+            dummy = dummy.next.next;
+        }
+        return start.next;
+    }
+    void swap(ListNode dummy){
+        ListNode first = dummy.next;
+        ListNode second = dummy.next.next;
+        first.next = second.next;
+        second.next = first;
+        dummy.next = second;
+    }
+}
