24451c6057a9eb96842e7e0f42bd458ad79f15a3
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-04-12 23:29:46 -0700

    Backtracking - Subset.java

--- Backtracking - Subset.java
+++ Backtracking - Subset.java
@@ -1,27 +1,37 @@
 /*
 https://leetcode.com/problems/subsets/
 Given a set of distinct integers, nums, return all possible subsets (the power set).
-
 Note: The solution set must not contain duplicate subsets.
 Example:
 Input: nums = [1,2,3]
 Output:
 [ [3], [1], [2], [1,2,3], [1,3], [2,3], [1,2], [] ]
+*/
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, 0);
+        return list;
+    }
 
-While iterating through all numbers, for each new number, we can either pick it or not pick it
-1, if pick, just add current number to every existing subset.
-2, if not pick, just leave all existing subsets as they are.
-We just combine both into our result.
-
+    private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+        list.add(new ArrayList<>(tempList));
+        for(int i = start; i < nums.length; i++){
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
+/*
 For example, {1,2,3} intially we have an emtpy set as result [ [ ] ]
 Considering 1, if not use it, still [ ], if use 1, add it to [ ], so we have [1] now
 Combine them, now we have [ [ ], [1] ] as all possible subset
 
 Next considering 2, if not use it, we still have [ [ ], [1] ], if use 2, just add 2 to each previous subset, we have [2], [1,2]
 Combine them, now we have [ [ ], [1], [2], [1,2] ] 
-*/
-class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
+public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<List<Integer>>(); 
         ans.add(new ArrayList());
         for(int n : nums){
@@ -44,4 +54,4 @@ class Solution {
         }
         return ans;
     }
-}
+*/
