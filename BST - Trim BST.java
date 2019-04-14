63b747bdc6abccb4bade68ddf31bbcf2e988a667
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-04-14 01:10:37 -0700

    BST - Trim BST.java

--- BST - Trim BST.java
+++ BST - Trim BST.java
@@ -11,10 +11,11 @@ Input:
   1
 L = 1  R = 3
 Output: 
-      3
-     / 
-   2 
-
+    3
+   / 
+  2 
+ /
+1
 
 L=32 R=44
   45
@@ -28,27 +29,12 @@ o/p: 36
 */
 class Solution {
     public TreeNode trimBST(TreeNode root, int L, int R) {
-        
-        // Find a valid root which is used to return. For following recursive function this code block is not necessary 
-        // this will be handled automatically in helper function. 
-        while (root!=null && (root.val < L || root.val > R)) {
-            if (root.val < L) 
-                root = root.right;
-            
-            if (root.val > R) 
-                root = root.left;
-        }
-
-        return trimBSTHelper(root,L,R);
-    }
-    
-    public TreeNode trimBSTHelper(TreeNode root, int L, int R) {
         if (root == null) 
             return null;
         
-        if (root.val < L) 
+        if (root.val < L)  // Find a valid root which be returned. 
             return trimBSTHelper(root.right, L, R);
-        if (root.val > R) 
+        if (root.val > R) // Find a valid root which will be returned. 
             return trimBSTHelper(root.left, L, R);
         
         root.left = trimBSTHelper(root.left, L, R);
@@ -71,28 +57,21 @@ class Solution {
         
         if(root==null) return null;
         
-        trimLeftBST(root,L);
-        trimRightBST(root,R);
+        trimBSTHelper(root,L,R);
         return root;
     }
     
-    public TreeNode trimLeftBST(TreeNode root, int L) {
-        if(root==null) return null;
-      
-        if(root.val<L)
-            return trimLeftBST(root.right,L);
-        else
-            root.left = trimLeftBST(root.left,L);
+    public TreeNode trimBSTHelper(TreeNode root, int L, int R) {
+        if (root == null) 
+            return null;
         
-        return root;
-    }
-    public TreeNode trimRightBST(TreeNode root, int R) {
-        if(root==null) return null;
+        if (root.val < L)  // Find a valid root which be returned. 
+            return trimBSTHelper(root.right, L, R);
+        if (root.val > R) // Find a valid root which will be returned. 
+            return trimBSTHelper(root.left, L, R);
         
-        if(root.val>R)
-            return trimRightBST(root.left,R);
-        else
-            root.right = trimRightBST(root.right,R);
+        root.left = trimBSTHelper(root.left, L, R);
+        root.right = trimBSTHelper(root.right, L, R);
         
         return root;
     }
