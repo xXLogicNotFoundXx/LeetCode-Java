d92b8e71728c44421a23e8242c0cdc9f66fb159e
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-04-15 01:00:48 -0700

    DP - Longest Increasing Subsequence.java

--- DP - Longest Increasing Subsequence.java
+++ DP - Longest Increasing Subsequence.java
@@ -25,3 +25,44 @@ class Solution {
         return max;
     }
 }
+
+/* nlogK  K is the ans.
+Solution 2 - Binary Search
+We can put the increasing sequence in a list.
+
+for each num in nums
+     if(list.size()==0)
+          add num to list
+     else if(num > last element in list)
+          add num to list
+     else 
+          replace the element in the list which is the smallest but bigger than num
+*/          
+public int lengthOfLIS(int[] nums) {
+    if(nums==null || nums.length==0)
+        return 0;
+ 
+    ArrayList<Integer> list = new ArrayList<Integer>(); 
+ 
+    for(int num: nums){
+        if(list.size()==0 || num>list.get(list.size()-1)){
+            list.add(num);
+        }else{
+            int i=0; 
+            int j=list.size()-1;
+ 
+            while(i<j){
+                int mid = (i+j)/2;
+                if(list.get(mid) < num){
+                    i=mid+1;
+                }else{
+                    j=mid;
+                }
+            }
+ 
+            list.set(j, num);
+        }
+    }
+ 
+    return list.size();
+}
