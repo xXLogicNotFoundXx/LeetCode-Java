e156f1cc4cf33a714f6f81892783cb84e25355ec
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-04-16 01:07:50 -0700

    DSG : shortest word distance.java

--- DSG : shortest word distance.java
+++ DSG : shortest word distance.java
@@ -1,14 +1,19 @@
 https://leetcode.com/articles/shortest-word-distance-ii/
+Design a class which receives a list of words in the constructor, and implements a method 
+that takes two words word1 and word2 and return the shortest distance between these two words in the list.
+Your method shortest(String word1, String word2 will be called repeatedly many times with different parameters. 
 
-	class WordDistance {
+class WordDistance {
         Map<String, ArrayList<Integer>> locations = new HashMap<String, ArrayList<Integer>>();
-        public WordDistance(String[] words) {
+        
+	public WordDistance(String[] words) {
             for (int i = 0; i < words.length; i++) {
                 ArrayList<Integer> loc = locations.getOrDefault(words[i], new ArrayList<Integer>());   // getOrDefault
                 loc.add(i);
                 locations.put(words[i], loc);
             }
         }
+	
         public int shortest(String word1, String word2) {
             ArrayList<Integer> loc1, loc2;
             // Location lists for both the words the indices will be in SORTED order by default
