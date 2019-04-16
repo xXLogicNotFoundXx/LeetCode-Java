2c9378e334b27a2cca3af7d6c7e653a67b5e07df
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-04-16 00:50:08 -0700

    SlidingWindow - Longest substring with At Most K Distinct Chars.java

--- SlidingWindow - Longest substring with At Most K Distinct Chars.java
+++ SlidingWindow - Longest substring with At Most K Distinct Chars.java
@@ -1,18 +1,25 @@
+https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
 Given a string, find the longest substring that contains only two unique characters.
 Given "abcadcacacaca" and 3, it returns "cadcacacaca".
 
-public class LongestSubstringAtMostKDistinct {
-    public static int LongestSubstringAtMostKDistinctElement(String s, int K){
+public class Solution {
+    public int lengthOfLongestSubstringKDistinct(String s, int K){
+        if(K<=0) 
+            return 0;
+        
         Map<Character,Integer> map = new HashMap<Character, Integer>();
         int maxLen = 0;
         int start =0;
+        
         for(int i=0;i<s.length();i++){
             int count = map.getOrDefault(s.charAt(i),0);
             map.put(s.charAt(i),count+1);
-            if(map.size()==K)
+            
+            if(map.size()==K) {    // remember size may stay K for next chars so you have to calcualte this again.
                 maxLen = Math.max(maxLen,i-start+1); // +1 bcz we are including the current index too
+            }
+            
             if(map.size()>K){
-                maxLen = Math.max(maxLen,i-start);  // no +1 because we are not the current index bcz it is extra char
                 while(map.size()>K) {
                     int existingCharCount = map.get(s.charAt(start)) -1;
                     if (existingCharCount == 0) {
@@ -24,9 +31,6 @@ public class LongestSubstringAtMostKDistinct {
                 }
             }
         }
-       return maxLen;
-    }
-    public static void main(String[] args) {
-        System.out.println(LongestSubstringAtMostKDistinct("abcccadcacacaca",1));
+       return maxLen==0 ? s.length() : maxLen; // may be the whole string didnt hit the K distinct chars
     }
 }
