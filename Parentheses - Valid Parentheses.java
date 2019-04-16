7e328e85e747fd6d1c2252e73bceec9ccfbe5f0d
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-04-15 20:31:44 -0700

    Parentheses - Valid Parentheses.java

--- Parentheses - Valid Parentheses.java
+++ Parentheses - Valid Parentheses.java
@@ -6,6 +6,21 @@ Input: "{[]}"
 Output: true
 
 class Solution {
+// Very smart 
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
    /* public boolean isValid(String s) {
         Stack<Character> stack = new Stack<Character>();
         for(int i=0;i<s.length();i++){
@@ -23,19 +38,5 @@ class Solution {
             return true;
         return false;
     } */
-    // smart 
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<Character>();
-        for (char c : s.toCharArray()) {
-            if (c == '(')
-                stack.push(')');
-            else if (c == '{')
-                stack.push('}');
-            else if (c == '[')
-                stack.push(']');
-            else if (stack.isEmpty() || stack.pop() != c)
-                return false;
-        }
-        return stack.isEmpty();
-    }
+    
 }
