6ef4e69c49d1441e87529ec30c5fe7d69cd9d859
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-05-05 18:08:04 -0700

    3SUM.java

--- 3SUM.java
+++ 3SUM.java
@@ -5,31 +5,34 @@ Find all unique triplets in the array which gives the sum of zero.
 The solution set must not contain duplicate triplets.
 */
 class Solution {
-    public List<List<Integer>> threeSum(int[] num) {
-        Arrays.sort(num);
-        List<List<Integer>> res = new ArrayList<>(); 
-        for (int i = 0; i < num.length-2; i++) {
-            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
-                int lo = i+1, hi = num.length-1, sum = 0 - num[i];
-                while (lo < hi) {
-                    if (num[lo] + num[hi] < sum){
-                        lo++;
-                    }else if (num[lo] + num[hi] > sum){
-                        hi--;
-                    }else{
-                        List<Integer> set = new ArrayList<Integer>();
-                        set.add(num[i]);
-                        set.add(num[lo]);
-                        set.add(num[hi]);
-                        res.add(set);
-                        while (lo < hi && num[lo] == set.get(1)) lo++;
-                        // we dont need to change hi bcz low is changed 
-                        // so hi will change naturally 
-                        // while (lo < hi && num[hi] == set.get(2)) hi--;
-                    } 
-               }
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        if(nums==null) return ans;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+            while(i!=0 && i<nums.length-2 && nums[i] == nums[i-1])
+                i++;
+            
+            int left=i+1,right=nums.length-1;
+            while(left<right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum==0){
+                    List<Integer> subAns =  new ArrayList<>();
+                    subAns.add(nums[i]);
+                    subAns.add(nums[left]);
+                    subAns.add(nums[right]);
+                    ans.add(subAns);
+                    left++;
+                    while(left<right && nums[left] == nums[left-1]) 
+                        left++;
+                    
+                }else if(sum<0)
+                    left++;
+                else 
+                    right--;
             }
+            
         }
-        return res;
+        return ans;
     }
 }
