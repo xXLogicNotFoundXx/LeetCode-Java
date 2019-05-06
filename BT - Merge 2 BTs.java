1b09294842ca113b79ddee0cd9116e261adca88b
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-05-06 10:34:11 -0700

    BT - Merge 2 BTs.java

--- BT - Merge 2 BTs.java
+++ BT - Merge 2 BTs.java
@@ -33,18 +33,13 @@ Merged tree:
  */
 class Solution {
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        if(t1==null) return t2;
-        if(t2==null) return t1;
-        return mergeTrees1(t1,t2);
-    }
-    public TreeNode mergeTrees1(TreeNode t1, TreeNode t2) {
         if(t2==null)
             return t1;
         if(t1==null)
             return t2;
         t1.val = t1.val+t2.val;
-        t1.left = mergeTrees1(t1.left,t2.left);
-        t1.right = mergeTrees1(t1.right,t2.right);
+        t1.left = mergeTrees(t1.left,t2.left);
+        t1.right = mergeTrees(t1.right,t2.right);
         return t1;
     }
 }
