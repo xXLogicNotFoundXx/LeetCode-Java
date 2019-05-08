b0c773c5475588c973cb76a3644168d2653d7153
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-05-08 02:45:05 -0700

    2D - Set Zeros.java

---
+++ 2D - Set Zeros.java
@@ -0,0 +1,44 @@
+/*
+https://leetcode.com/problems/set-matrix-zeroes/
+Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
+*/
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean setFirstRowToZeroes = false;
+        boolean setFirstColumnToZeroes = false;
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                    if(i == 0) 
+                        setFirstRowToZeroes = true;
+                    if(j == 0) 
+                        setFirstColumnToZeroes = true;
+                }
+            }
+        }
+
+        for(int i = 1; i < matrix.length; i++) {
+            for(int j = 1; j < matrix[0].length; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(setFirstRowToZeroes) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(setFirstColumnToZeroes) {
+            for(int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+    }
+}
