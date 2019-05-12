04bb35ce2205b55270653beb52a4767d5a857267
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-05-11 23:05:45 -0700

    Backtracking - Subset.java

--- Backtracking - Subset.java
+++ Backtracking - Subset.java
@@ -10,7 +10,6 @@ Output:
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(nums);
         backtrack(list, new ArrayList<>(), nums, 0);
         return list;
     }
