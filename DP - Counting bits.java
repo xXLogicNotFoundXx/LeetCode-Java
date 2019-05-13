8710b34f858fa013825ce3f1dd103bcadf99771d
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-05-12 19:54:32 -0700

    DP - Counting bits.java

--- DP - Counting bits.java
+++ DP - Counting bits.java
@@ -6,13 +6,14 @@ Input: 2
 Output: [0,1,1]
 Example 2:
 
-Input: 5
-Output: [0,1,1,2,1,2]
-// pattern is same after 2, 4, 8, 16 have 1 
-// a[n] = 1 
-// or 
-// a[n] = a[n/2] + a[n%2]; 
+Input: 8
+Output: [0,1,1,2,1,2,2,3,1]
+        
+// 2, 4, 8, 16 have only one 1  
+// a[n] = a[n/2] + n%2; 
 class Solution {
+    // 2, 4, 8, 16 have only one 1  
+    // a[n] = a[n/2] + n%2; 
     public int[] countBits(int num) {
         if(num<=0) 
             return new int[]{0}; // num <=0 should return [0] 
@@ -20,15 +21,10 @@ class Solution {
         int [] dp = new int[num+1];
         dp[0]=0;
         dp[1]=1;
-        int sqr = 2;
-        for(int i=2;i<=num;i++){  // here i though i%2==0 then dp[i]=1 which is wrong because 6%2 is 0.
-            if(i==sqr){
-                dp[i] =1;
-                sqr *=2;
-            } else{
-                dp[i] = dp[i/2] + dp[i%2]; 
-            }
+        for(int i=2;i<=num;i++){  
+           dp[i] = dp[i/2] + i%2;  
         }
         return dp;
     }
 }
+
