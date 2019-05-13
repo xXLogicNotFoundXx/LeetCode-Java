0437f81d9bfdcd9ebec0af206d8b817e9d6adf0e
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-05-12 21:44:57 -0700

    DP - Longest Palindromic Subsequence.java

--- DP - Longest Palindromic Subsequence.java
+++ DP - Longest Palindromic Subsequence.java
@@ -1,13 +1,20 @@
 /*
 https://leetcode.com/problems/longest-palindromic-subsequence/
-516. Longest Palindromic Subsequence
+516. Longest Palindromic Subsequence .  
+Subsequence mean you can drop some characters in the string 
 Given a string s, find the longest palindromic subsequence's length in s. 
 You may assume that the maximum length of s is 1000.
 Input:"bbbab"
-Output:4
-One possible longest palindromic subsequence is "bbbb".
+Output:4    bcz we can form palindrom "bbbb".
 Input:"bcbd"
 Output:2
+
+i=0 and j=n-1 
+if the chars are same 
+    then call recurse (i+1,j-1)
+else
+    recurse (i+1,j) OR recurse (i,j-1) ( drop start char OR end char) 
+
 */
 public class Solution {
     public int longestPalindromeSubseq(String s) {
