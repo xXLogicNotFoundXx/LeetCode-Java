41ee6463ae713f2200ce572a8544b8f8481eee14
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-05-25 01:18:39 -0700

    Stack - Daily Temperatures find next warm.java

--- Stack - Daily Temperatures find next warm.java
+++ Stack - Daily Temperatures find next warm.java
@@ -1,3 +1,4 @@
+/*
 https://leetcode.com/articles/daily-temperatures/
 Given a list of daily temperatures T, return a list such that, for each day in the input, 
 tells you how many days you would have to wait until a warmer temperature. 
@@ -6,7 +7,7 @@ If there is no future day for which this is possible, put 0 instead.
 For example, given the list of temperatures 
 T  =  [73, 74, 75, 71, 69, 72, 76, 73], your output should be 
 O/p = [1, 1, 4, 2, 1, 1, 0, 0].
-
+*/
 class Solution {
     public int[] dailyTemperatures(int[] T) {
         int []res = new int[T.length];
