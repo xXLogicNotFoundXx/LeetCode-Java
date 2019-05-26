7864feb0b15bb240fc8050165f870634d73b2d14
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-05-25 23:37:22 -0700

    DP - Subarray Sum K.java

--- DP - Subarray Sum K.java
+++ DP - Subarray Sum K.java
@@ -3,6 +3,7 @@ Continuous - Subarray Sum Equals K
 https://leetcode.com/problems/subarray-sum-equals-k/
 Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
 
+Prefix Sum strategy  
 Example 1:
 Input:nums = [1,1,1], k = 2
 Output: 2
