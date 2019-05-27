cd319bf5719de6891777919a1837ac50a94c216f
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-05-27 16:51:47 -0700

    Create 2D - Simple.java

---
+++ 2D - Simple.java
@@ -0,0 +1,23 @@
+/*
+https://leetcode.com/problems/toeplitz-matrix/
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+  [1,2,3,4],
+  [5,1,2,3],
+  [9,5,1,2]
+Output: True
+Explanation:
+In the above grid, the diagonals are:
+"[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+In each diagonal all elements are the same, so the answer is True.
+*/
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length - 1; i++) {
+            for (int j = 0; j < matrix[i].length - 1; j++) {
+                if (matrix[i][j] != matrix[i + 1][j + 1]) return false;
+            }
+        }
+        return true;
+    }
+}
