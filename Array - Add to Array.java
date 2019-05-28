e78e77d39fc12b211f55b1273dc90180c3b4b6d4
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-05-27 18:53:50 -0700

    Update Array - Add to Array.java

--- Array - Add to Array.java
+++ Array - Add to Array.java
@@ -2,11 +2,12 @@ https://leetcode.com/problems/add-to-array-form-of-integer/submissions/
 [1,2,0,0] + 34 => [1,2,3,4]
 [1,1,1] + 9 => [1,0,0,0]
 
+// This is very good example and has so many corner cases following is the best way to do it.
 class Solution {
     public List<Integer> addToArrayForm(int[] A, int K) {
         for(int i = A.length-1; i>=0 && K!=0; i--){
             int temp = A[i] + K;
-            A[i] = temp % 10;
+            A[i] = temp % 10;        // IMP A has the result not in the list.
             K = temp / 10;
         }
         List<Integer> B = new ArrayList<Integer>();
@@ -15,7 +16,8 @@ class Solution {
             K = K/10;
         }
        
-        for(int i : A)
+        // remeber our reslut is still in A we have to add that in a list 
+        for(int i : A) 
             B.add(i);
         
         return B;
