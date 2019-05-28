094585a95fb99ab01a8637639b295a4ed05f351d
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-05-27 19:10:47 -0700

    2D - Rotate Array.java

--- 2D - Rotate Array.java
+++ 2D - Rotate Array.java
@@ -11,7 +11,7 @@ swap(matrix[i][j], matrix[i][matrix.length-1-j])
 class Solution {
     public void rotate(int[][] matrix) {
         for(int i = 0; i<matrix.length; i++){
-            for(int j = i; j<matrix[0].length; j++){
+            for(int j = i; j<matrix[0].length; j++){ .  // see j=i only half the matrxi is traversed 
                 int temp = 0;
                 temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
@@ -19,7 +19,7 @@ class Solution {
             }
         }
         for(int i =0 ; i<matrix.length; i++){
-            for(int j = 0; j<matrix.length/2; j++){
+            for(int j = 0; j<matrix.length/2; j++){ // see j<matrix.length/2 only half the matrxi is traversed 
                 int temp = 0;
                 temp = matrix[i][j];
                 matrix[i][j] = matrix[i][matrix.length-1-j];
