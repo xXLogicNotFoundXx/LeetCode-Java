ced01ed52ec583c568f76737b1ce0e3e03556d41
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-05-31 13:43:37 -0700

    Sort - Wiggle Sort.java

--- Sort - Wiggle Sort.java
+++ Sort - Wiggle Sort.java
@@ -7,7 +7,8 @@ Input: nums = [3,5,2,1,6,4]
 Output: One possible answer is [3,5,1,6,2,4]
 */
 class Solution {
-    public void wiggleSort(int[] nums) {
+    // O(nlogn)
+    public void wiggleSort1(int[] nums) {
         PriorityQueue<Integer> pqMin = new PriorityQueue<>();
         PriorityQueue<Integer> pqMax = new PriorityQueue<>(Collections.reverseOrder());
         for(int i : nums){
@@ -23,4 +24,26 @@ class Solution {
                 nums[i++] = pqMin.poll(); 
         }
     }
+    // O(n)
+    public void wiggleSort(int[] nums) {
+        boolean less = true;
+        for (int i=0; i<nums.length-1; i++) {
+            if (less) {
+                if (nums[i]>nums[i+1])
+                    swap(nums, i, i+1);
+                }
+            else {
+                if (nums[i]<nums[i+1])
+                    swap(nums, i, i+1);
+            }
+            less = !less;
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+       int temp = nums[i];
+        nums[i]= nums[j];
+        nums[j] = temp;
+    }
+    
 }
