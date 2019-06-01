4e1a1142b36f2ee02c5380a534194f490d87c332
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-06-01 12:00:55 -0700

    Subarray - Subarray Sums Divisible by K.java

--- Subarray - Subarray Sums Divisible by K.java
+++ Subarray - Subarray Sums Divisible by K.java
@@ -5,7 +5,33 @@ Input: A = [4,5,0,-2,-3,1], K = 5
 Output: 7
 Explanation: There are 7 subarrays with a sum divisible by K = 5:
 [4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+
+We can all agree that for an array int[] A, where N = len(A), that there are N prefix sums.
+prefix[0] = A[0], prefix[1] = A[0] + A[1], ... prefix[i] = A[0] + ... + A[i].
+
+Then to calculate how many subarrays are divisible by K is logically equivalent to saying, how many ways can we pair up all prefix sum pairs (i,j) where i < j
+such that (prefix[j] - prefix[i]) % K == 0.
+
+Just from that information alone we easily get a O(n^2) solution.
+Compute all prefix sums, then check all pair to see if k divides the difference between them.
+
+However, if we just exploit some information w.r.t to the remainder of each prefix sum we can manipulate this into a linear algorithm. Here's how.
+
+Number Theory Part
+I noted above that we need to find all prefix sum pairs (i,j) such tha (p[j] - p[i]) % K == 0.
+But this is only true, if and only if p[j] % K == p[i] % K
+Why is this?
+
+According the the division algorithm we can express p[j] and p[i] in the following way.
+p[j] = bK + r0 where 0 <= r0 < K
+p[i] = aK + r1 where 0<= r1 < K
+
+Then p[j] - p[i] = (b*K + r0) - (a*K + r1)
+= b*K - a*K + r0 - r1 = K*(b-a) + r0 - r1
+Again: p[j] - p[i] = K*(b-a) + (r0-r1), in other words
+K only divides p[j] - p[i] iff r0-r1 = 0 <-> r0 = r1 QED
 */
+
 class Solution {
     // Prefix Sum is one technique we need to solve this problem, but it's not enough. 
     // we dont know the exact numbr we need to it becomes N^2TLE 
