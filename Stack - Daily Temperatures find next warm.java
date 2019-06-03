b53ab5c3503041f4bb8d39d5f2d00c9afcf98e28
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-06-03 01:10:00 -0700

    Update Stack - Daily Temperatures find next warm.java

--- Stack - Daily Temperatures find next warm.java
+++ Stack - Daily Temperatures find next warm.java
@@ -8,27 +8,15 @@ For example, given the list of temperatures
 T  =  [73, 74, 75, 71, 69, 72, 76, 73], your output should be 
 O/p = [1, 1, 4, 2, 1, 1, 0, 0].
 */
-class Solution {
-    public int[] dailyTemperatures(int[] T) {
-        int []res = new int[T.length];
-        Stack<Integer> temp = new Stack<Integer>();
-        for(int i=T.length-1;i>=0;i--){
-            if(temp.empty()){
-                res[i]=0;
-            } else { 
-                while(!temp.isEmpty()){
-                    if(T[temp.peek()] > T[i]){
-                        res[i]=temp.peek()-i;
-                        break;
-                    }
-                    temp.pop();
-                }
-                if(temp.isEmpty()){
-                    res[i]=0;
-                }
-            }
-            temp.push(i);
+public int[] dailyTemperatures(int[] temperatures) {
+    Stack<Integer> stack = new Stack<>();
+    int[] ret = new int[temperatures.length];
+    for(int i = 0; i < temperatures.length; i++) {
+        while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+            int idx = stack.pop();
+            ret[idx] = i - idx;
         }
-        return res;
+        stack.push(i);
     }
+    return ret;
 }
