3190fa10d0ba00e7247f56bdff01fd97570a0da8
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-06-04 23:22:14 -0700

    Update DP - Coin Change.java

--- DP - Coin Change.java
+++ DP - Coin Change.java
@@ -1,6 +1,12 @@
+/*
 https://leetcode.com/problems/coin-change/submissions/
-// [2,4,5] 12
-// ans = 3 
+You are given coins of different denominations and a total amount of money amount.
+Write a function to compute the fewest number of coins that you need to make up that amount.
+If that amount of money cannot be made up by any combination of the coins, return -1.
+Input: coins = [1, 2, 5], amount = 11
+Output: 3 
+Explanation: 11 = 5 + 5 + 1
+*/
 class Solution {
     public int coinChange(int[] coins, int amount) {
         if(coins==null || coins.length==0)
