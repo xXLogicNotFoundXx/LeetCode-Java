ee33d528d1225a115db39efc05aa6dfe47f86015
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-06-04 23:04:50 -0700

    Update DP - Climbing Stairs.java

--- DP - Climbing Stairs.java
+++ DP - Climbing Stairs.java
@@ -13,4 +13,24 @@ class Solution {
         return a[0]; 
     }   
 }
-
+/*
+https://leetcode.com/problems/min-cost-climbing-stairs/
+Your goal is to reach endofthearray+1 with min cost. 
+1st and 2nd cost is not pais until you jump from that position. 
+Input: cost = [10, 15, 20]                  
+Output: 15
+Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
+*/
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        if(cost==null)
+            return 0;
+        int c =0;
+        for(int i=2;i<=cost.length;i++){
+            c = Math.min(cost[i-1],cost[i-2]);
+            if(i!=cost.length)
+                cost[i] += c;
+        }
+        return c;
+    }
+}
