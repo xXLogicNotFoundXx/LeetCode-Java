807658b2e41e2cc9b78bea4d30ff8ca60e9c2493
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-06-12 23:11:13 -0700

    Update SQL queries.sql

--- SQL/SQL queries.sql
+++ SQL/SQL queries.sql
@@ -1,9 +1,9 @@
-Second Highest salary :  https://leetcode.com/problems/second-highest-salary/submissions/
+--Second Highest salary :  https://leetcode.com/problems/second-highest-salary/submissions/
   Select max(salary) as SecondHighestSalary
   from Employee
   where salary < (select max(salary) from Employee);
 
-Nth highest Salary  : https://leetcode.com/problems/nth-highest-salary/ 
+--Nth highest Salary  : https://leetcode.com/problems/nth-highest-salary/ 
   CREATE FUNCTION getNthHighestSalary(N IN NUMBER) RETURN NUMBER IS
   result NUMBER;
   BEGIN
@@ -14,16 +14,36 @@ Nth highest Salary  : https://leetcode.com/problems/nth-highest-salary/
       RETURN result;
   END;
  
-Employees Earning More Than Their Managers : 
+--Employees Earning More Than Their Managers : https://leetcode.com/problems/employees-earning-more-than-their-managers/
   Select a.Name
   From Employee a 
   JOIN Employee b
   ON a.ManagerId = b.Id
   Where a.Salary > b.Salary;
 
-Customers Who Never Order
+-- Customers Who Never Ordered : https://leetcode.com/problems/customers-who-never-order/
   Select C.Name As Customers
   From Customers C 
   left Join Orders O
   On C.Id = O.CustomerId
   Where O.CustomerId is null;
+
+--  Classes More Than 5 Students : https://leetcode.com/problems/classes-more-than-5-students/
+  select class 
+  from courses
+  group by class
+  having count(distinct student)>=5;
+  
+-- 178. Rank Scores  : https://leetcode.com/problems/rank-scores/
+  SELECT Score, (SELECT count(distinct Score) FROM Scores WHERE Score >= s.Score) Rank
+  FROM Scores s
+  ORDER BY Score desc;
+
+-- 184. Department Highest Salary
+  
+-- find average salary for each department with more than five members
+  select DEPARTMENT,count(STAFF_ID) as CountStaff, avg(SALARY) as AVGSalary
+  from STAFF
+  group by DEPARTMENT
+  having count(STAFF_ID) > 5
+  
