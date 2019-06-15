6ce22c80f3de6f98e39879022db071a37b2bc348
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-06-15 14:43:26 -0700

    Update HEAP - K closest points.java

--- HEAP - K closest points.java
+++ HEAP - K closest points.java
@@ -1,9 +1,21 @@
 // https://leetcode.com/problems/k-closest-points-to-origin/
-
-// we can use MAX_HEAP and keep K items in the heap at anygiven point. (O(n*log(k))
-// if we use MIN_HEAP then we have to add all points in HEAP and then take out K elements. (O(n*log(n))
-// if size becomes more than K we remove item from the HEAP in the end we gonna have K point which are closest to 0,0
 class Solution {
+    /* 1 NLogN sort the array and return K 
+    public int[][] kClosest(int[][] points, int K) {
+        Arrays.sort(points, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] p1, int[] p2) {
+                return p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1];
+            }
+        });
+        return Arrays.copyOfRange(points, 0, K);
+    }
+    */
+    // 2 
+    // we can use MAX_HEAP and keep K items in the heap at anygiven point
+    // Theoretically, the time complexity is O(NlogK), 24ms
+    // The advantage of this solution is it can deal with real-time(online) stream data. It does not have to know the size of the data previously.
+
     class PointDistance implements Comparator<PointDistance>{
         int x,y,distance;
         PointDistance(int x, int y){
@@ -17,10 +29,8 @@ class Solution {
         }
     }
     
-    public int[][] kClosest(int[][] points, int K) {
-        if(points == null) 
-            return new int[0][];
-        if(points.length <=K) 
+    public int[][] kClosest1(int[][] points, int K) {
+        if(points == null || points.length <=K) 
             return points;
         
         PriorityQueue<PointDistance> queue = new PriorityQueue<PointDistance>(new PointDistance());
@@ -30,7 +40,7 @@ class Solution {
                 queue.poll();
         }
         
-        int [][] ans = new int[K][2];
+        int [][] ans = new int[queue.size()][2];
         int i=0;
         while(!queue.isEmpty()){
             PointDistance p = queue.poll();
@@ -38,5 +48,55 @@ class Solution {
             ans[i++][1] = p.y;
         }
         return ans;
+    
+    }
+    /*
+    III. The last solution is based on quick sort, we can also call it quick select. 
+    In the quick sort, we will always choose a pivot to compare with other elements. 
+    After one iteration, we will get an array that all elements smaller than the pivot are on the left side of the pivot
+    and all elements greater than the pivot are on the right side of the pviot (assuming we sort the array in ascending order). 
+    average time complexity O(N) - 5ms 
+    */
+    public int[][] kClosest(int[][] points, int K) {
+        if(points == null || points.length <=K) 
+            return points;
+        
+        int start = 0, end = points.length-1;
+        while(start<end){
+            int partitionIndex = partitionQuickSelect(points,start,end);
+            if(partitionIndex==K){
+                return Arrays.copyOfRange(points, 0, K);
+            } 
+            if(partitionIndex<K)
+                start = partitionIndex+1;
+            else
+                end= partitionIndex-1;
+        }
+        
+        return Arrays.copyOfRange(points, 0, K);
+    }
+    
+    int partitionQuickSelect(int[][] points, int start, int end){
+        int []pivot = points[end];
+        int partitionIndex = start;
+        
+        for(int i=start;i<end;i++){
+            if(distanceToOrigin(points[i]) <= distanceToOrigin(pivot) ){
+                swap(points,partitionIndex,i);
+                partitionIndex++;
+            }
+        }
+        swap(points,partitionIndex,end);
+        return partitionIndex; 
+    }
+    
+    void swap(int[][] points, int i, int j){
+        int []temp = points[i];
+        points[i] = points[j];
+        points[j] = temp;
+    }
+    
+    int distanceToOrigin(int []p1){
+        return p1[0]*p1[0]+p1[1]*p1[1];
     }
 }
