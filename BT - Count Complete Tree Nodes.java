fc8c283c1ce5d47d4f22bf2d446c87797b3adcd9
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-06-16 00:05:25 -0700

    BT - Count Complete Tree Nodes.java

---
+++ BT - Count Complete Tree Nodes.java
@@ -0,0 +1,36 @@
+/**
+222. Count Complete Tree Nodes
+https://leetcode.com/problems/count-complete-tree-nodes/
+as every recursion is log(n) overall is O(log(n)^2) 
+Also , n + n-1 + n-2 + ... 1 = (n*(n-1)/2) => O(n^2) 
+here in this n is log(n) so O(log(n)^2) 
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        int leftDepth = leftDepth(root);
+        int rightDepth = rightDepth(root);
+
+        if (leftDepth == rightDepth)
+            return (int)Math.pow(2,leftDepth) - 1;
+        else
+            return 1+countNodes(root.left) + countNodes(root.right);
+    }
+
+    private int rightDepth(TreeNode root) {
+        int dep = 0;
+        while (root != null) {
+            root = root.right;
+            dep++;
+        }
+        return dep;
+    }
+
+    private int leftDepth(TreeNode root) {
+        int dep = 0;
+        while (root != null) {
+            root = root.left;
+            dep++;
+        }
+        return dep;
+    }
+}
