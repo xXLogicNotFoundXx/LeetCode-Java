0d743e9650a08dc38285db8e69c918fc6c56bd0c
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-06-23 13:51:59 -0700

    Update Stack - Basic Calculator ().java

--- Stack - Basic Calculator ().java
+++ Stack - Basic Calculator ().java
@@ -14,39 +14,65 @@ Output: 23
 */
 class Solution {
     public int calculate(String s) {
+         if(s==null || s.isEmpty())
+            return 0;
+        // get rid of the spaces 
+        // s = s.replaceAll("\\s+","");
         
-        int number = 0; 
-        Stack<Integer> st = new Stack<>();
-        int result=0;
-        int sign=1;
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(Character.isDigit(ch)){
-                number *= 10;
-                number += ch-'0';
-            }
-            if(ch=='+'){
-                result += sign*number;
-                number=0;
-                sign = 1 ;
-            }else if( ch=='-'){
-                result += sign*number;
-                number=0;
-                sign = -1 ;
-            }else if( ch=='('){
-                st.push(result);
-                st.push(sign);
-                result=0;
-                sign=1;
-            }else if( ch==')'){
-                result += sign * number;  
-                number = 0;
-                result *= st.pop();    //stack.pop() is the sign before the parenthesis
-                result += st.pop();   //stack.pop() now is the result calculated before the parenthesis
-            }
+        Queue<Character> q = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            q.offer(c);
         }
-        if(number!=0)
-            result += sign*number;
-        return result;
+        q.offer('+'); // IMP very good trick 
+        
+        return calculate(q);
+    }
+    
+    public int calculate(Queue<Character> q) {
+        Stack<Integer> st = new Stack<Integer>();
+        int num=0;
+        char prevSign = '+';
+        
+        while(!q.isEmpty()){
+            char ch = q.poll();
+            
+            if(ch== ' ')
+                continue;
+            
+            if(ch>='0' && ch <='9'){
+                num = (num*10) + (ch-'0');
+            } 
+            
+            if(ch==')' || ch=='+'|| ch=='-'){ // very IMP that we have ) in this 
+                switch(prevSign){
+                    case '+' :
+                        st.push(num);
+                        break;
+                    case '-' :
+                        st.push(num*-1);
+                        break;
+                }
+                num=0;
+                prevSign = ch;
+            }
+            
+            if(ch=='('){
+                // remember there is a sign before this 
+                // that means num is 0 here and sign is on prevSign
+                num = calculate(q);
+            }
+            
+            if(ch==')'){
+                // we made sure we calculate result before 
+                break;
+            }
+            
+        }
+        
+        int total = 0;
+        while(!st.isEmpty())
+            total += st.pop();
+        
+        return total;
     } 
 }
