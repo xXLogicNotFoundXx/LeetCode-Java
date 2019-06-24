db4434ab00ac95ac4d0321e6b99ab542e86000b6
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-06-23 21:56:55 -0700

    Update Array - Interval list intersection.java

--- Array - Interval list intersection.java
+++ Array - Interval list intersection.java
@@ -1,7 +1,9 @@
 /*
 https://leetcode.com/problems/interval-list-intersections/
 Given two lists of closed intervals, each list of intervals is pairwise disjoint and in sorted order.
-Input: A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
+Input: 
+A = [[0,2],[5,10],[13,23],[24,25]], 
+B = [[1,5],[8,12],[15,24],[25,26]]
 Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
 Reminder: The inputs and the desired output are lists of Interval objects, and not arrays or lists.
 */
