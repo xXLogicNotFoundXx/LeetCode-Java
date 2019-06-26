5312b9bc4c029385bf820a9095076ca9e8cc2abe
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2019-06-26 00:51:16 -0700

    Update String - 101.java

--- String - 101.java
+++ String - 101.java
@@ -1,3 +1,41 @@
+/*
+https://leetcode.com/problems/isomorphic-strings/
+Given two strings s and t, determine if they are isomorphic.
+Two strings are isomorphic if the characters in s can be replaced to get t.
+Input: s = "egg", t = "add"
+Output: true
+
+String 1:              A B E A C D B
+index pattern:         0 1 2 0 4 5 1
+String 2:              X Y I X H K Y
+index pattern:         0 1 2 0 4 5 1
+Whether the two strings are isomorphic can be judged by the index patterns. 
+In the above example, these two strings are isomorphic with the same index patterns.
+*/
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s == null || t == null) return false;
+        if (s.length() != t.length()) return false;
+        
+        Map<Character, Integer> mapS = new HashMap<Character, Integer>();
+        Map<Character, Integer> mapT = new HashMap<Character, Integer>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            int indexS = mapS.getOrDefault(s.charAt(i), -1);
+            int indexT = mapT.getOrDefault(t.charAt(i), -1);
+                        
+            if (indexS != indexT) 
+                return false;
+            
+            mapS.put(s.charAt(i), i);
+            mapT.put(t.charAt(i), i);
+        }
+        
+        return true;
+    }
+}
+
 https://leetcode.com/problems/subdomain-visit-count/
 When we visit a domain like "discuss.leetcode.com", we will also visit the parent domains "leetcode.com" and "com" implicitly.
 Input:  ["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]
