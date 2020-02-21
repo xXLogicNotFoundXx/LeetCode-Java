acd5b2862410021105c1cb406c1ce76e00e94019
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-02-21 15:23:07 -0800

    Update 2D - Rotate Array.java

--- 2D - Rotate Array.java
+++ 2D - Rotate Array.java
@@ -1,6 +1,8 @@
-https://leetcode.com/problems/rotate-image/
 
-/*swap the symmetry  then swap lefft <-> right.
+
+/*
+https://leetcode.com/problems/rotate-image/
+swap the symmetry  then swap lefft <-> right.
 1  2  3    1  4  7    7  4  1     
 4  5  6 => 2  5  8 => 8  5  2
 7  8  9    3  6  9    9  6  3
@@ -45,25 +47,25 @@ OR
     * 1 2 3     3 2 1     3 6 9
     * 4 5 6  => 6 5 4  => 2 5 8
     * 7 8 9     9 8 7     1 4 7
-    
-    public void rotate(int[][] matrix) {
-        int n=matrix.length;
-        for(int i=0;i<n/2;i++,i++){
-            for(int j=0;j<n;j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[n-1-i][j];
-                matrix[n-1-i][j] = temp;
-            }
-        }
-        
-        for(int i = 0; i<matrix.length; i++){
-            for(int j = i; j<matrix[0].length; j++){
-                int temp = 0;
-                temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
+*/
+public void rotate(int[][] matrix) {
+    int n=matrix.length;
+    for(int i=0;i<n/2;i++,i++){
+        for(int j=0;j<n;j++){
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[n-1-i][j];
+            matrix[n-1-i][j] = temp;
         }
     }
+
+    for(int i = 0; i<matrix.length; i++){
+        for(int j = i; j<matrix[0].length; j++){
+            int temp = 0;
+            temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+}
 
 
