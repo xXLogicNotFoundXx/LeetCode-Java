a7370f0da31ef9eea1304181bbab4df6966656b6
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-02-21 13:59:23 -0800

    Update DP - 2D unique-paths to right bottom cell.java

--- DP - 2D unique-paths to right bottom cell.java
+++ DP - 2D unique-paths to right bottom cell.java
@@ -1,5 +1,6 @@
 /*
 https://leetcode.com/problems/unique-paths/
+Problem 1: 
 A robot is located at the top-left corner of a m x n grid.
 The robot can only move either down or right at any point in time. 
 The robot is trying to reach the bottom-right corner of the grid.
@@ -34,5 +35,41 @@ class Solution {
             }
         }
         return dp[n-1][m-1];
+    }
+}
+
+/*
+https://leetcode.com/problems/unique-paths-ii/
+Problem 2: 
+if some obstacles are added to the grids. How many unique paths would there be?
+Input:
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+Output: 2
+1. Right -> Right -> Down -> Down
+2. Down -> Down -> Right -> Right
+*/
+ public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        obstacleGrid[0][0] = (obstacleGrid[0][0]==1)? 0 : 1;
+
+        for(int i = 1;i<m;i++)
+            obstacleGrid[i][0] = (obstacleGrid[i][0]==1)? 0 : obstacleGrid[i-1][0];
+
+        for(int j = 1;j<n;j++)
+            obstacleGrid[0][j] = (obstacleGrid[0][j]==1)? 0 : obstacleGrid[0][j-1];
+
+        for(int i = 1;i<m;i++){
+            for(int j =1;j<n;j++){
+                obstacleGrid[i][j] = (obstacleGrid[i][j]==1)? 0 : obstacleGrid[i-1][j]+obstacleGrid[i][j-1];
+            }
+        }
+        return obstacleGrid[m-1][n-1];
     }
 }
