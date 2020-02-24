146702b2d14a15cd58d22c4b97798477d044d76e
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-02-24 15:09:12 -0800

    Update 3SUM.java

--- 3SUM.java
+++ 3SUM.java
@@ -36,3 +36,43 @@ class Solution {
         return ans;
     }
 }
+
+/*
+3 SUM closest 
+https://leetcode.com/problems/3sum-closest/
+Given array nums = [-1, 2, 1, -4], and target = 1.
+The sum that is closest to the target is 2. 
+(-1 + 2 + 1 = 2).
+
+*/
+
+class Solution {
+     public int threeSumClosest(int[] nums, int target) {
+        if(nums==null) return 0;
+        Arrays.sort(nums);
+        int diff = Integer.MAX_VALUE;
+        int ansSum = 0;
+        for(int i=0;i<nums.length-2;i++){
+            while(i!=0 && i<nums.length-2 && nums[i] == nums[i-1])
+                i++;
+            
+            int left=i+1,right=nums.length-1;
+            while(left<right){
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if(diff > Math.abs(sum-target)){
+                    diff = Math.abs(sum-target);
+                    ansSum = sum;
+                } 
+                
+                if(sum==target){
+                    return sum;
+                }else if(sum<target)
+                    left++;
+                else 
+                    right--;
+            }  
+        }
+        return ansSum;
+    }
+}
