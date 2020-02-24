15b9c17d75df7fb415260ccd76a800069dceded1
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-02-24 15:38:51 -0800

    Update 3SUM.java

--- 3SUM.java
+++ 3SUM.java
@@ -76,3 +76,33 @@ class Solution {
         return ansSum;
     }
 }
+
+  
+/*
+https://leetcode.com/problems/3sum-with-multiplicity/
+Given an integer array A, and an integer target, return the number of tuples i, j, k  
+such that i < j < k and A[i] + A[j] + A[k] == target.
+As the answer can be very large, return it modulo 10^9 + 7.
+Time : O(n^2)
+Space : O(n^2)
+To calculate combinations, we will use the formula C(n,r)   nCr = n!/(r!*(n-r)!), Also noted as C(n,r).
+        
+*/
+class Solution {
+    // This one is N^2 takes  599 ms 
+    public int threeSumMulti(int[] A, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        int res = 0;
+        for (int i = 0; i < A.length; i++) {
+            // A[i] must be considered for result before we calculate all combination sum and we dont 
+            // want to consider that sum bcz that includesA[i]
+            res = (res + map.getOrDefault(target - A[i], 0)) % 1000000007;
+            for (int j = 0; j < i; j++) {
+                int temp = A[i] + A[j];
+                map.put(temp, map.getOrDefault(temp, 0) + 1);
+            }
+        }
+        return res;
+    }
+}
