472633f419b2ed08129dfaf10f3d29aab9694e84
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-02-25 12:27:48 -0800

    Update 2SUM.java

--- 2SUM.java
+++ 2SUM.java
@@ -1,10 +1,12 @@
 
+/*
 https://leetcode.com/problems/two-sum/
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 Given nums = [2, 7, 11, 15], target = 9,
 return [0, 1]
 // We do target-nums[i] and put that in the map with index 
 // if we find that number going through loop we found the two indices  
+*/
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
@@ -18,3 +20,22 @@ class Solution {
         return null;
     }
 }
+/*
+Given an array of integers, how many pairs are there whose sum is less than K.
+n*Log(n)
+*/
+int findSmallerPairs(int arr[], int K) { 
+    Arrays.sort(arr);
+    int l = 0, r = n - 1; 
+    int result = 0; 
+
+    while (l < r){ 
+        if (arr[l] + arr[r] < K){ 
+            result += (r - l); 
+            l++; 
+        } else
+            r--; 
+    } 
+
+    return result; 
+} 
