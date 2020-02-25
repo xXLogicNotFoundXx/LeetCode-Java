3db817d5ebb4f2b2c14e2fae4ba17d2a2e060219
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-02-24 16:57:20 -0800

    Update 3SUM.java

--- 3SUM.java
+++ 3SUM.java
@@ -88,7 +88,7 @@ where n represents the total number of items, and r represents the number of ite
 public int threeSumMulti(int[] A, int target) {
     // This one is worst N^2  but takes  60ms 
     Arrays.sort(A);
-    int m = 1000000007;
+    
     int res = 0;
     for (int i = 0; i < A.length - 2; ++i) {
         int j = i + 1;
@@ -101,7 +101,7 @@ public int threeSumMulti(int[] A, int target) {
                     // If A[j...k] are all equal, then there are combinations of C(k - j + 1, 2) 
                     // combinations that meet the requirement.
                     // let this sink in so we did n*n-1/2   ... n-2! is cancelled out look at the formula.
-                    res = (res + (k - j + 1) * (k - j) / 2) % m; 
+                    res +=  (k - j + 1) * (k - j) / 2; 
                     break;
                 }
                 int l = 1, r = 1;
@@ -111,7 +111,7 @@ public int threeSumMulti(int[] A, int target) {
                 while (j < k - r && A[k - r] == A[k]) 
                     r++; // r: number of elements equal to A[k].
 
-                res = (res + l * r) % m; // found l * r cases that meet the requirement.
+                res +=  l * r); // found l * r cases that meet the requirement.
                 j += l; // forward j by l steps.
                 k -= r; // backward k by r steps.
             }
