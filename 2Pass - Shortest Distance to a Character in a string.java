7e44817817066e7c273c5b25858a8b38a8acc015
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-02-28 09:55:44 -0800

    Update 2Pass - Shortest Distance to a Character in a string.java

--- 2Pass - Shortest Distance to a Character in a string.java
+++ 2Pass - Shortest Distance to a Character in a string.java
@@ -1,4 +1,11 @@
 // https://leetcode.com/problems/shortest-distance-to-a-character/
+/*
+Given a string S and a character C, 
+return an array of integers representing the shortest distance from the character C in the string.
+
+Input: S = "loveleetcode", C = 'e'
+Output: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+*/
 class Solution {
     public int[] shortestToChar(String S, char C) {
         int[] res = new int[S.length()];
