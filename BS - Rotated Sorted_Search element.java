9af35cb6fd2ced5ce8c19ed24de9c78e472edd5e
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-03-02 09:41:51 -0800

    Update BS - Rotated Sorted_Search element.java

--- BS - Rotated Sorted_Search element.java
+++ BS - Rotated Sorted_Search element.java
@@ -1,4 +1,4 @@
-Problem 1 : without duplicates in an array 
+//Problem 1 : without duplicates in an array 
 /*
 https://leetcode.com/problems/search-in-rotated-sorted-array/
 [4,5,6,7,0,1,2]).
@@ -32,3 +32,42 @@ class Solution {
     }
 }
 
+//Problem 2 : With duplicates in an array 
+/*
+https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+With duplicates in the an input array it will be slightly different 
+But that slight variation can put this algo to O(n).
+I tried following same as Rotated array logic without duplication but it fails for condtiong where i can not decide which partition to go 
+if element at low , mid and high are same where do you go? so we need extra condition for that 
+if that is true we can do low++ and high-- 
+Input : 1 1 1 1 1 1 7 1 1
+*/
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int low =0, high = nums.length-1;
+        while(low<=high){
+            
+            int mid = (low+high)/2;
+            if(nums[mid]==target)
+                return true;
+            
+            if( nums[low] == nums[mid] && nums[mid] == nums[high] ) {
+                low++; high--;
+                continue;               // Important
+            }
+            
+            if(nums[low]<=nums[mid]){ // low and mid could be the same but not high i/p [3,1] targer 1 
+                if( target < nums[mid] && nums[low]<=target)   // <= is important 
+                    high=mid-1;
+                else
+                    low=mid+1;
+            }else{
+                if(nums[mid]<target && target <=nums[high])     // <= is important
+                    low=mid+1;
+                else
+                    high=mid-1;
+            }
+        }
+        return false; 
+    }
+}
