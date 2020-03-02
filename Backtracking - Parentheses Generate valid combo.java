687c9e93b58785bf767d77dae825716c650f8869
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-03-02 12:59:18 -0800

    Update Backtracking - Parentheses Generate valid combo.java

--- Backtracking - Parentheses Generate valid combo.java
+++ Backtracking - Parentheses Generate valid combo.java
@@ -1,3 +1,4 @@
+/*
 https://leetcode.com/problems/generate-parentheses/
 For example, given n = 3, a solution set is:
 [
@@ -8,24 +9,39 @@ For example, given n = 3, a solution set is:
   "()()()"
 ]
 
+Time Complexity: O(2^n)
+As in each position, there're 2 candidates to put: '(' or ')'. For n positions, there're 2^n possibilities.
+Space Complexity: O(2^n)
+
+However, the official LeetCode solutions there is a very difficult mathematical proof to get a tighter bound 
+on the time/space complexity, by using "Catalan" numbers. 
+
+*/
+
 class Solution {
-  public List<String> generateParenthesis(int n) {
-        List<String> list = new ArrayList<String>();
-        DFS_Paranthesis(list, "(", 1, 0, n);
-        return list;
- }
- 
- public void DFS_Paranthesis(List<String> list, String str, 
-				             int open, int close, int max){
- 
+     public List<String> generateParenthesis(int n) {
+           List<String> list = new ArrayList<String>();
+           DFS_Paranthesis(list, new StringBuffer(""), 0, 0, n);
+           return list;
+     }
+    
+     public void DFS_Paranthesis(List<String> list, 
+                                 StringBuffer str, 
+                                 int open, int close, int max){
+
         if(str.length() == max*2){
-            list.add(str);
+            list.add(str.toString());
             return;
         }
- 
-        if(open < max)
-            DFS_Paranthesis(list, str+"(", open+1, close, max);
-        if(close < open)
-            DFS_Paranthesis(list, str+")", open, close+1, max);
- }
+
+        if(open < max){
+            DFS_Paranthesis(list, str.append("("), open+1, close, max);
+            str.setLength(str.length()-1);
+        }
+         
+        if(close < open){
+            DFS_Paranthesis(list, str.append(")"), open, close+1, max);
+            str.setLength(str.length()-1);
+        }
+     }
 }
