abd14a3167cb384e681b47eab2f0448aa23fbf22
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-03-02 09:32:48 -0800

    Update Backtracking - CombinationsSum.java

--- Backtracking - CombinationsSum.java
+++ Backtracking - CombinationsSum.java
@@ -8,7 +8,8 @@ A solution set is:
 ]
 All numbers (including target) will be positive integers.
 The solution set must not contain duplicate combinations.
-all elements in the candidate array are distinct and the array size is n 
+all elements in the candidate array are distinct and the array size is n.
+
 C(n,1) + C(n,2) + ... + C(n,n) = 2^n - C(n,0) = O(2^n)
 
 */
@@ -32,6 +33,60 @@ class Solution {
             for(int i=start;i<candidates.length;i++){
                 subAns.add(candidates[i]);
                 combinationSumHelper(candidates,target - candidates[i],Ans,subAns,i); // not i + 1 because we can reuse same elements
+                subAns.remove(subAns.size()-1);
+            }
+        }
+    }
+}
+
+
+/*
+Combination Sum II with duplucates 
+https://leetcode.com/problems/combination-sum-ii/
+
+Given a collection of candidate numbers (candidates) and a target number (target), 
+find all unique combinations in candidates where the candidate numbers sums to target.
+Each number in candidates may only be used once in the combination.
+
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+Example 1:
+Input: candidates = [10,1,2,7,6,1,5], target = 8,
+A solution set is:
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+
+time complexity = O(n!)
+space O(m) m is the size of the Ans array 
+*/
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> Ans  = new ArrayList<List<Integer>>();
+        List<Integer> subAns = new ArrayList<Integer>();
+        combinationSumHelper(candidates,target,Ans,subAns,0);
+        return Ans;
+    }
+    
+    void combinationSumHelper(int[] candidates, int target, List<List<Integer>> Ans, List<Integer> subAns, int start){
+        if(target == 0){
+            Ans.add(new ArrayList<Integer>(subAns));
+            return;
+        }
+        
+        if(target > 0){
+            for(int i=start;i<candidates.length;i++){
+                if(target - candidates[i] < 0) // this is good because we dont want to process all the array
+                    break;
+                if(i>start && candidates[i]==candidates[i-1]) 
+                    continue;
+                subAns.add(candidates[i]);
+                combinationSumHelper(candidates,target - candidates[i],Ans,subAns,i+1);
                 subAns.remove(subAns.size()-1);
             }
         }
