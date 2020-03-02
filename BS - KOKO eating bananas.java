d7cd20571cbe007cc43f4a1c364a22f82634a8a1
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-03-02 12:13:04 -0800

    Update BS - KOKO eating bananas.java

--- BS - KOKO eating bananas.java
+++ BS - KOKO eating bananas.java
@@ -5,6 +5,9 @@ There are N piles of bananas, the i-th pile has piles[i] bananas.
 The guards have gone and will come back in H hours.
 If the pile has more than K bananas, she eats K and then next hr she eat K from the same pile.
 If the pile has less than K bananas, she eats all of them and won't eat any more bananas during this hour.
+Koko likes to eat slowly, but still wants to finish eating all the bananas before the guards come back.
+
+Return the minimum integer K such that she can eat all the bananas within H hours.
 */
 // Note: the max she can eat in an hr is  one pile she can not eat 2 piles in an hr
 // so the max she can eat is maxPile in an hr 
