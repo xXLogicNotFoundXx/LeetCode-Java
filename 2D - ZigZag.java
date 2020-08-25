b880c041256366a3a3b308fc30f1d8ba159247a6
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-08-25 01:07:56 -0700

    Update 2D - ZigZag.java

--- 2D - ZigZag.java
+++ 2D - ZigZag.java
@@ -7,9 +7,41 @@ Given a matrix of M x N elements (M rows, N columns), return all elements of the
 Output:  [1,2,4,7,5,3,6,8,9]
 */
 class Solution {
+      public int[] findDiagonalOrder(int[][] matrix) {
+         if (matrix == null || matrix.length == 0) return new int[0];
+
+         int row = 0, col = 0, pos = 0, m = matrix.length, n=matrix[0].length, output [] = new int[m * n];
+
+         for (pos = 0; pos < m * n; pos++) {
+             output[pos] = matrix[row][col];
+
+             if ((row + col) % 2 == 0) {
+                 // The direction is always up when the sum of row & col is even
+
+                 // For last column, go down
+                 if (col == n-1) { row++; }                
+
+                 // For first row & non-last columns, go right
+                 else if (row == 0) { col++; }
+
+                 // For not first row & non-last columns, go up and to the right
+                 else { row--; col++; }
+
+             } else {
+                 // The direction is always down when the sum of row & col is odd
+
+                 // For last row, go right
+                 if (row == m-1) { col++; } 
+
+                 //  For non-last row & first column, go down
+                 else if (col == 0) { row++; }
+
+                 // For non-last row & non-first column, go down and to the left
+                 else { row++; col--; }
+             }
+        }
+     }
      /*
-        I tried traverasl switch with havinf layer and then layer 0,2,4 going up
-        and layers 1,3,5 going down it became to complicated.
         So travering in one direction only i.e. do up only and then reverting result set whenver necessary.
       */
     public int[] findDiagonalOrder(int[][] matrix) {
