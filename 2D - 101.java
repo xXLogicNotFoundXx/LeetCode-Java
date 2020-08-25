9c671e2a59161aaea123b36e83d8ed8f072cc711
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-08-25 00:14:38 -0700

    Update 2D - 101.java

--- 2D - 101.java
+++ 2D - 101.java
@@ -51,28 +51,25 @@ Rotate the image by 90 degrees (Cloclwise)
 7  8  9      9  6  3
 */
 class Solution {
-      /*.  Solution One 
+ /*.  Solution One 
       1. swap the symmetry 
       2. swap left <-> right
-
       1  2  3      1  4  7     7  4  1     
       4  5  6  =>  2  5  8  => 8  5  2
       7  8  9      3  6  9     9  6  3
       */
     public void rotate(int[][] matrix) {
         for(int i = 0; i<matrix.length; i++){
-            for(int j = i; j<matrix[0].length; j++){ .  // see j=i only half the matrxi is traversed 
-                int temp = 0;
-                temp = matrix[i][j];
+            for(int j = i; j<matrix[0].length; j++){   // j=i only half the matrxi is traversed 
+                int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
         for(int i =0 ; i<matrix.length; i++){
-            for(int j = 0; j<matrix.length/2; j++){ // see j<matrix.length/2 only half the matrix is traversed 
-                int temp = 0;
-                temp = matrix[i][j];
-                matrix[i][j] = matrix[i][matrix.length-1-j];
+            for(int j = 0; j<matrix[0].length/2; j++){ // only half the matrix is traversed 
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1-j]; // very IMP calculations
                 matrix[i][matrix.length-1-j] = temp;   // very IMP calculations
             }
         }
@@ -87,12 +84,11 @@ class Solution {
       7 8 9      1 2 3     9 6 3
     */
     public void rotate(int[][] matrix) {
-        int n=matrix.length;
-        for(int i=0;i<n/2;i++,i++){
-            for(int j=0;j<n;j++){
+        for(int i=0; i<matrix.length/2; i++){
+            for(int j=0; j<matrix[0].length; j++){
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[n-1-i][j];
-                matrix[n-1-i][j] = temp;
+                matrix[i][j] = matrix[matrix.length-1-i][j];
+                matrix[matrix.length-1-i][j] = temp;
             }
         }
 
@@ -107,8 +103,16 @@ class Solution {
 }
 
 /*  Anticlockwise rotate 90 degree
+  Solution 1  
+    1. swap symetry 
+    2. swap up<->down
     
-    1. first reverse left to right,
+    1  2  3      1  4  7      3 6 9    
+    4  5  6  =>  2  5  8  =>  2 5 8
+    7  8  9      3  6  9      1 4 7
+    
+  Solution 2   
+    1. first reverse left <-> right,
     2. swap the symmetry
     
     1 2 3     3 2 1     3 6 9
@@ -167,6 +171,7 @@ B
 Output:
      |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
 AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+                  | 0 0 1 |
 */
 public class Solution {
     public int[][] multiply(int[][] A, int[][] B) {
