4c50d447916221e8c169fe21e28cce0750028283
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-08-25 01:14:07 -0700

    Update 2D - ZigZag.java

--- 2D - ZigZag.java
+++ 2D - ZigZag.java
@@ -8,15 +8,17 @@ Output:  [1,2,4,7,5,3,6,8,9]
 */
 class Solution {
       public int[] findDiagonalOrder(int[][] matrix) {
-         if (matrix == null || matrix.length == 0) return new int[0];
+        if (matrix == null || matrix.length == 0) 
+            return new int[0];
 
-         int row = 0, col = 0, pos = 0, m = matrix.length, n=matrix[0].length, output [] = new int[m * n];
+        int row = 0, col = 0, pos = 0, m = matrix.length, n=matrix[0].length, output [] = new int[m * n];
 
-         for (pos = 0; pos < m * n; pos++) {
+        for (pos = 0; pos < m * n; pos++) {
              output[pos] = matrix[row][col];
 
              if ((row + col) % 2 == 0) {
                  // The direction is always up when the sum of row & col is even
+                 // The order of if else condition is also very important 
 
                  // For last column, go down
                  if (col == n-1) { row++; }                
@@ -29,7 +31,8 @@ class Solution {
 
              } else {
                  // The direction is always down when the sum of row & col is odd
-
+                 // The order of if else condition is also very important 
+                 
                  // For last row, go right
                  if (row == m-1) { col++; } 
 
@@ -40,7 +43,9 @@ class Solution {
                  else { row++; col--; }
              }
         }
+        return output;
      }
+ 
      /*
         So travering in one direction only i.e. do up only and then reverting result set whenver necessary.
       */
