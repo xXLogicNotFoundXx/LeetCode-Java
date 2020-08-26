25f6b15a16944a4377def79d2b5610506e1b5cd9
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-08-26 00:55:27 -0700

    Update 2D - Consecutive Ones.java

--- 2D - Consecutive Ones.java
+++ 2D - Consecutive Ones.java
@@ -29,7 +29,7 @@ class Solution {
                     max = Math.max(max,row[i]);
                     max = Math.max(max,col[j]);
                     max = Math.max(max,slash[i+j]);
-                    max = Math.max(max,backslash[j-i+M.length]);
+                    max = Math.max(max,backslash[j-i+M.length]); // -i+M.length will never be negative ( i can go to max M.length)
                 }else{
                     row[i] = 0;
                     col[j] = 0;
@@ -39,5 +39,56 @@ class Solution {
             }
         }
         return max;
+    }
+}
+
+
+/*
+Similar problem where you have to calculate unique number for antiDiagonal
+1329. Sort the Matrix Diagonally
+https://leetcode.com/problems/sort-the-matrix-diagonally/
+    [3,3,1,1]       [1,1,1,1]
+    [2,2,1,2]   =>  [1,2,2,2]
+    [1,1,1,2]       [1,2,3,3]
+*/
+class Solution {
+
+    // Time O(MNlogD), where D is the length of diagonal with D = min(M,N).
+    // Space O(MN)
+    public int[][] diagonalSort(int[][] mat) {
+        int m = mat.length, n= mat[0].length;
+        PriorityQueue<Integer>[] pArray = new PriorityQueue[m+n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(pArray[j-i+m]==null) // -i+m will never be negative ( i can go to max m)
+                    pArray[j-i+m] = new PriorityQueue<Integer>();
+                pArray[j-i+m].add(mat[i][j]);
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                mat[i][j]= pArray[j-i+m].poll();
+            }
+        }
+        return mat;
+    }
+    
+    // Solution 2 
+    // Here if we are not storing in an array we dont have to worry about negative index 
+    // so j-i+mat.length is not required 
+    // i-j would give same number for given antidiagonal elements with index i,j
+     public int[][] diagonalSort2(int[][] A) {
+        int m = A.length, n = A[0].length;
+        HashMap<Integer, PriorityQueue<Integer>> d = new HashMap<>();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                d.putIfAbsent(i - j, new PriorityQueue<>());    // nice putIfAbsent
+                d.get(i - j).add(A[i][j]);
+            }
+        }
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                A[i][j] = d.get(i - j).poll();
+        return A;
     }
 }
