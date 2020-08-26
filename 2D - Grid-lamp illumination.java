14b2993bcd12493b30d8c062ec6147af02b831ef
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-08-25 21:31:00 -0700

    Update 2D - Grid-lamp illumination.java

--- 2D - Grid-lamp illumination.java
+++ 2D - Grid-lamp illumination.java
@@ -6,12 +6,11 @@ After each query (x, y) [in the order given by queries], we turn off any lamps t
 Return an array of integers ans where each value ans[i] should be equal to the answer of the ith query queries[i].
 */
 class Solution1 {
-
     // Have an array of Row, Col, Diagonal and AntiDiagonal 
-    // Keep a count of lamps in each row[i] ,col[j] ,diag[i+j], antidiag[j-i+N]
+    // Keep a count of lamps in each row[i] ,col[j] ,diag[i+j], antidiag[j-i+N] (remember this calculation to get same index for antiDiagonal elements)
     // if any of these arrays count is more than 0 for given i,j then that cell is lit
     // then traverse the block and update the count of these arrays
-    // memory limit exceed bcz we creat a grid of NxN where N=10000
+    // memory limit exceed bcz we creat a grid of NxN where N=10000 and all these 4 arrays with N 
     public int[] gridIllumination(int N, int[][] lamps, int[][] queries) {
         int[][] lampsGrid = new int[N][N];
         int rN = N, cN = N;
@@ -67,25 +66,27 @@ class Solution1 {
     }
 }
 
+// This one is nice solution. Using minimal memory. 
+// The logic is same but using HashMaps and Set
 class Solution {
     
     public int[] gridIllumination(int N, int[][] lamps, int[][] queries) {
-        Map<Integer, Integer> m1 = new HashMap();       // row number to count of lamps
-        Map<Integer, Integer> m2 = new HashMap();       // col number to count of lamps
-        Map<Integer, Integer> m3 = new HashMap();       // AntiDiagonal(Backslash) x-y to count of lamps
-        Map<Integer, Integer> m4 = new HashMap();       // diagonal x+y to count of lamps
-        Set<String> m5 = new HashSet<>();                 //  lamp that are ON
+        Map<Integer, Integer> row = new HashMap();       // row number to count of lamps
+        Map<Integer, Integer> col = new HashMap();       // col number to count of lamps
+        Map<Integer, Integer> antiDiag = new HashMap();  // AntiDiagonal(Backslash) y-x+N to count of lamps
+        Map<Integer, Integer> diag = new HashMap();      // diagonal x+y to count of lamps
+        Set<String> litLamps = new HashSet<>();          //  lamps that are ON
         
         
         // increment counters while adding lamps
         for(int i=0; i<lamps.length; i++){
             int x = lamps[i][0];
             int y = lamps[i][1];
-            m1.put(x,   m1.getOrDefault(x, 0) + 1);
-            m2.put(y,   m2.getOrDefault(y, 0) + 1);
-            m3.put(x-y, m3.getOrDefault(x-y, 0) + 1); // AntiDiagonal(Backslash) x-y to count of lamps
-            m4.put(x+y, m4.getOrDefault(x+y, 0) + 1); // diagonal x+y to count of lamps
-            m5.add(x+""+y);
+            row.put(x, row.getOrDefault(x, 0) + 1);
+            col.put(y, col.getOrDefault(y, 0) + 1);
+            antiDiag.put(y-x+N, antiDiag.getOrDefault(y-x+N, 0) + 1); // AntiDiagonal(Backslash) y-x+N to count of lamps
+            diag.put(x+y, diag.getOrDefault(x+y, 0) + 1); // diagonal x+y to count of lamps
+            litLamps.add(x+"-"+y);
         }
         
         int[] ans = new int[queries.length];
@@ -98,26 +99,25 @@ class Solution {
             int x = queries[i][0];
             int y = queries[i][1];
             
-            ans[i] = (m1.getOrDefault(x, 0) > 0 ||
-                      m2.getOrDefault(y, 0) > 0 || 
-                      m3.getOrDefault(x-y, 0) > 0 || 
-                      m4.getOrDefault(x+y, 0) > 0) ? 1 : 0;            
+            ans[i] = (row.getOrDefault(x, 0) > 0 ||
+                      col.getOrDefault(y, 0) > 0 || 
+                      antiDiag.getOrDefault(y-x+N, 0) > 0 || 
+                      diag.getOrDefault(x+y, 0) > 0) ? 1 : 0;            
             
             // switch off the lamps in 9 blocks if any 
             for(int d=0; d<dirs.length; d++){
                 int x1 = x + dirs[d][0];
                 int y1 = y + dirs[d][1];
-				if(m5.contains(x1+""+y1)){
+				if(litLamps.contains(x1+"-"+y1)){
                     // the lamp is on, turn it off, so decrement the count of the lamps
-                    m1.put(x1, m1.getOrDefault(x1, 1) - 1);
-                    m2.put(y1, m2.getOrDefault(y1, 1) - 1);
-                    m3.put(x1 - y1, m3.getOrDefault(x1 - y1, 1) - 1);
-                    m4.put(x1 + y1, m4.getOrDefault(x1 + y1, 1) - 1);
-                    m5.remove(x1+""+y1);
+                    row.put(x1, row.get(x1) - 1);
+                    col.put(y1, col.get(y1) - 1);
+                    antiDiag.put(y1-x1+N, antiDiag.get(y1-x1+N) - 1);
+                    diag.put(x1+y1, diag.get(x1+y1) - 1);
+                    litLamps.remove(x1+"-"+y1);
                 }
             }
         }
-        
         return ans;
     }
 }
