0e9043b2290675e988df44a3bca044a776f483c8
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-08-25 17:17:33 -0700

    Update DP - 101.java

--- DP - 101.java
+++ DP - 101.java
@@ -1,3 +1,87 @@
+/* https://leetcode.com/problems/climbing-stairs/solution/
+You are climbing a stair case. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ */
+class Solution {
+    // left -> right 
+    public int climbStairs(int n) {
+        if(n==0) 
+            return 0;
+        int[] dp = new int[n+1];
+        dp[0]=dp[1]=1;
+        for(int i=2;i<n+1;i++){
+            dp[i] = dp[i-1]+ dp[i-2];
+        }
+        return dp[n];
+    }
+  
+   // right -> left 
+    public int climbStairs(int n) {
+        if(n==0)
+            return 0;
+        int []a = new int[n+1];
+        Arrays.fill(a,0);
+        a[n]=1;
+        a[n-1]=1;
+        for(int i=n-2;i>=0;i--){
+            a[i] = a[i+1] + a[i+2]; // different ways of climbing ith step
+        }
+        return a[0]; 
+    }   
+}
+
+/*
+https://leetcode.com/problems/min-cost-climbing-stairs/
+Your goal is to reach endofthearray+1 with min cost. 
+1st and 2nd cost is not pais until you jump from that position. 
+Input: cost = [10, 15, 20]                  
+Output: 15
+Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
+*/
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        if(cost==null)
+            return 0;
+        int c =0;
+        for(int i=2;i<=cost.length;i++){
+            c = Math.min(cost[i-1],cost[i-2]);
+            if(i!=cost.length)
+                cost[i] += c;
+        }
+        return c;
+    }
+}
+
+  
+/*
+https://leetcode.com/problems/coin-change
+You are given coins of different denominations and a total amount of money amount.
+Write a function to compute the fewest number of coins that you need to make up that amount.
+If that amount of money cannot be made up by any combination of the coins, return -1.
+Input: coins = [1, 2, 5], amount = 11
+Output: 3 
+Explanation: 11 = 5 + 5 + 1
+*/
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if(coins==null || coins.length==0)
+            return -1;
+        int []dp = new int[amount+1];
+        Arrays.fill(dp,-1);
+        dp[0]=0;
+        for(int coin : coins){
+            for(int i=coin;i<=amount;i++){
+                if(dp[i-coin]!=-1){
+                    int coinNum = dp[i-coin]+1;
+                    dp[i] = dp[i]==-1 ? coinNum : Math.min(dp[i],coinNum);
+                } 
+            }
+        }
+        return dp[amount];
+    }
+}
+
+
 /*
 https://leetcode.com/problems/minimum-cost-for-tickets/
 The days of the year that you will travel is given as an array days.  Each day is an integer from 1 to 365.
