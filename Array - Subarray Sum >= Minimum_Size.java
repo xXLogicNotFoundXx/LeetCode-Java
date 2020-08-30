85cfee6e35370ef015936a412edb69e6f337eef8
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-08-29 18:11:18 -0700

    Update Array - Subarray Sum >= Minimum_Size.java

--- Array - Subarray Sum >= Minimum_Size.java
+++ Array - Subarray Sum >= Minimum_Size.java
@@ -7,20 +7,57 @@ find the minimal length of a contiguous subarray of which the ""sum ≥ s"". If 
 Ans : 3
 */
 class Solution {
-    public int minSubArrayLen(int sum, int[] nums) {
-        if (nums == null || nums.length == 0)
+    // My Solution O(N)
+    /*
+    We could keep 2 pointer,one for the start and another 
+    for the current subarray, 
+    and make optimal moves so as to keep the sum greater than s 
+    and maintain the lowest size possible.
+    */
+    public int minSubArrayLen1(int s, int[] nums) {
+        
+        if(nums==null||nums.length==0)
             return 0;
         
-        int total=0, min=nums.length+1;
-        int left=0, right=0;
-        while(right<nums.length){
-            total += nums[right++]; 
+        int total = nums[0]; // what if num[0] >=s
+        int minSubArray = total>=s ? 1 : Integer.MAX_VALUE; 
+        int left=0;
+        for(int i=1;i<nums.length;i++){
             
-            while(left<right && total>=sum){
-                min = Math.min(min,right-left);
-                total -= nums[left++];
+            total+=nums[i];
+            
+            if(total>=s && left<=i){
+                minSubArray = Math.min(minSubArray,i-left+1); // +1 bcz we added nums[i] already
+                
+                total-=nums[left];
+                left++;
+                
+                // we want left to keep moving forward & find min index diff 
+                total-=nums[i]; 
+                i--;
             }
         }
-        return min > nums.length ? 0 : min;
+        
+        return minSubArray == Integer.MAX_VALUE ? 0 : minSubArray;
+    }
+    
+    // another O(N)
+    public int minSubArrayLen(int s, int[] nums) {
+        if(nums==null||nums.length==0)
+            return 0;
+        
+        int total=0;
+        int left=0;
+        int minSubArray = Integer.MAX_VALUE; 
+        
+        for(int i=0;i<nums.length;i++){
+            total+=nums[i];
+            while(total>=s){
+                minSubArray =  Math.min(minSubArray,i-left+1); // +1 bcz we added nums[i] already
+                total-=nums[left];
+                left++;
+            }
+        }
+        return minSubArray == Integer.MAX_VALUE ? 0 : minSubArray;
     }
 }
