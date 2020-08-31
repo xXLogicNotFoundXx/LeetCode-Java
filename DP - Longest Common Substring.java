53ed0476a7d333b6224aaa3764648b37a55d55d9
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-08-30 17:20:59 -0700

    Update DP - Longest Common Substring.java

--- DP - Longest Common Substring.java
+++ DP - Longest Common Substring.java
@@ -6,11 +6,20 @@ bcda
 output = 3 
 
 https://www.youtube.com/watch?v=BysNXJHzCEs
+
+  Initial State                Final State
+  
+     a b c d f                  a b c d f  
+   0 0 0 0 0 0                0 0 0 0 0 0 
+ b 0                =>      b 0 0 1 0 0 0
+ c 0                        c 0 0 0 2 0 0
+ d 0                =>      d 0 0 0 0 3 0
+ a 0                        a 0 1 0 0 0 0 
 */
 
 class Solution {
     public int longestCommonSubstring(String A, String B) {
-        if(A == null||B == null) 
+        if(A == null || B == null) 
           return 0;
           
         int m = A.size();
@@ -18,8 +27,14 @@ class Solution {
         int max = 0;
         
         int[][] dp = new int[m + 1][n + 1];
-        for(int i = 1;i <= m;i++){
-            for(int j = 1;j <= n;j++){
+        for(int i = 0;i <= m;i++){
+            for(int j = 0;j <= n;j++){
+              
+              if(i == 0 || j == 0){
+                 dp[i][j] = 0;
+                 continue;   
+              }
+                
               if(A.charAt(i-1) == B.charAt(j-1)){
                   dp[i][j] = 1 + dp[i-1][j-1];
                   max = Math.max(max,dp[i][j]);
@@ -29,4 +44,3 @@ class Solution {
         return max;
     }
 }
-
