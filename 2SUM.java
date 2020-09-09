9fa42ffdcf1b7e665045d785ea6d6ababfb0c763
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-09-09 00:37:40 -0700

    Update 2SUM.java

--- 2SUM.java
+++ 2SUM.java
@@ -2,7 +2,7 @@
 /*
 https://leetcode.com/problems/two-sum/
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-Given nums = [2, 7, 11, 15], target = 9,
+Given nums = [2,-7,11,-2,15], target = -5,
 return [0, 1]
 // We do target-nums[i] and put that in the map with index 
 // if we find that number going through loop we found the two indices  
@@ -10,33 +10,23 @@ return [0, 1]
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        if(nums==null) return null;
-        Map<Integer,Integer> sumMap = new HashMap<Integer,Integer>(); 
+        if(nums==null) 
+            return null;
+        
+        Map<Integer,Integer> sumMap = new HashMap<>(); 
+        
         for(int i=0;i<nums.length;i++){
-            if(sumMap.containsKey(nums[i]))
-                return new int[]{sumMap.get(nums[i]),i};
+            
+            if(sumMap.containsKey(nums[i])){
+                int ans = new int[2];
+                ans[0]=sumMap.get(nums[i]);
+                ans[1]=i;
+                return ans;
+            }
+            
             sumMap.put(target-nums[i],i);
         }
         return null;
     }
 }
-/*
-https://leetcode.com/problems/two-sum-less-than-k/
-Given an array of integers, how many pairs are there whose sum is less than K.
-Time : n*Log(n)
-*/
-int findSmallerPairs(int arr[], int K) { 
-    Arrays.sort(arr);
-    int l = 0, r = n - 1; 
-    int result = 0; 
 
-    while (l < r){ 
-        if (arr[l] + arr[r] < K){ 
-            result += (r - l); 
-            l++; 
-        } else
-            r--; 
-    } 
-
-    return result; 
-} 
