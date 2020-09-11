d0c3a080b900ad8317493f6b95743182737af4b5
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-09-11 01:49:02 -0700

    101 Reverse integer value.java

--- 101 Reverse integer value.java
+++ 101 Reverse integer value.java
@@ -1,16 +1,21 @@
+// https://leetcode.com/problems/reverse-integer/
 class Solution {
-    public int reverse(int x) {
+    public int reverse1(int x) {
+        
         StringBuffer str = new StringBuffer();
         str.append(x);
         str.reverse();
+        
         int neg=1;
         if(str.charAt(str.length()-1) == '-'){
             str.deleteCharAt(str.length()-1);
             neg =-1;
         }
-        long l = Long.parseLong(str.toString()) * neg;
+        
+        long l = Long.valueOf(str.toString()) * neg;
         if(l > Integer.MAX_VALUE || l < Integer.MIN_VALUE)
             return 0;
-        return Integer.parseInt(str.toString())*neg;
+            
+        return Integer.valueOf(str.toString())*neg;
     }
 }
