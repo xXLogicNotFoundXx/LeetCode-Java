147247d882a867f2b9c869bce3b0ff5df93c14b6
sandeshkobal <40186122+sandeshkobal@users.noreply.github.com>
2020-09-11 01:10:40 -0700

    Update 2D - Pascals Triangle I and II.java

--- 2D - Pascals Triangle I and II.java
+++ 2D - Pascals Triangle I and II.java
@@ -7,19 +7,21 @@ class Solution {
     // 1 4 6 4 1
     // In this we can see it is always 1 when j==0 or j==i
     // when j>i then we compute the next row 
-    
-    public List<List<Integer>> generatePascalsTrangle(int numRows) {
+    public List<List<Integer>> generate(int numRows) {
         
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        int [][]result = new int[numRows][numRows];
+        
         for(int i=0;i<numRows;i++){
             List<Integer> oneRow = new ArrayList<Integer>();
+            
             for(int j=0; j<=i;j++){
-                if(j==0 || j==i)
-                    result[i][j] = 1;
-                else
-                    result[i][j] = result[i-1][j-1] + result[i-1][j];
-                oneRow.add(result[i][j]);
+                
+                if(j==0 || j==i){
+                    oneRow.add(1);
+                }else{
+                    List<Integer> lastRow = ans.get(i-1); 
+                    oneRow.add(lastRow.get(j-1) + lastRow.get(j));
+                }
             }
             ans.add(oneRow);
         }
@@ -29,28 +31,23 @@ class Solution {
 // https://leetcode.com/problems/pascals-triangle-ii/
 // Input: 3
 // Output: [1,3,3,1]  index start at 0  and only constant K space 
-
     public List<Integer> getRow(int rowIndex) {
-        List<Integer> oneRow = new ArrayList<Integer>();
-        if(rowIndex<0)
-            return oneRow;
-        int []result = new int[rowIndex+1];
-        int []prevResult = new int[rowIndex+1];
-        int []temp;
-        for(int i=0;i<rowIndex+1;i++){
+        List<Integer> ans = new ArrayList<Integer>();
+
+        for(int i=0;i<=rowIndex;i++){
+            List<Integer> tempRow = new ArrayList<Integer>();
             for(int j=0; j<=i;j++){
+                
                 if(j==0 || j==i)
-                    result[j] = 1;
+                    tempRow.add(1);
                 else{
-                    result[j] = prevResult[j-1] + prevResult[j];
+                    tempRow.add(ans.get(j-1) + ans.get(j));
                 }
+                
             }
-            temp = result;
-            result = prevResult;
-            prevResult = temp;
+            ans=tempRow;
         }
-        for(int j=0;j<prevResult.length;j++)
-            oneRow.add(prevResult[j]);
-        return oneRow; 
+
+        return ans; 
     }
 }
