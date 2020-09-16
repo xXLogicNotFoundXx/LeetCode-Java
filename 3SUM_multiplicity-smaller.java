4a44c388c27f1b16a22e4badd7afccdbe86dfa30
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2020-09-16 01:17:25 -0700

    Update 3SUM_multiplicity-smaller.java

--- 3SUM_multiplicity-smaller.java
+++ 3SUM_multiplicity-smaller.java
@@ -2,73 +2,32 @@
 https://leetcode.com/problems/3sum-with-multiplicity/
 Given an integer array A, and an integer target, return the number of tuples i, j, k  
 such that i < j < k and A[i] + A[j] + A[k] == target.
+
 As the answer can be very large, return it modulo 10^9 + 7.
+
 Time : O(n^2)
 Space : O(n^2)    
 */
 
 class Solution {
     // This one is N^2 takes  599 ms 
-    public int threeSumMulti1(int[] A, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int mod = 1000000007;
-        int res = 0;
-        for (int i = 0; i < A.length; i++) {
-            // A[i] must be considered for result before we calculate all combination sum and we dont 
-            // want to consider that sum bcz that includesA[i]
-            res = (res + map.getOrDefault(target - A[i], 0)) % mod;
-            for (int j = 0; j < i; j++) {
-                int temp = A[i] + A[j];
-                map.put(temp, map.getOrDefault(temp, 0) + 1);
-            }
-        }
-        return res;
-    }
-    
-/*    Runtime 39 ms. This is same as 3SUm combinations.
-Time complexity: O(N^2)
-Space complexity: O(1)
-You might think that the sorting of numbers will mess up the order but it really doesnt matter. 
-For any expression a + b + c = T, you can always change the order of the arguments so that their indices conform to the requirement.
-For example, a + b + c = T can be rewritten to a + c + b = T if index of b is greater than index of c.
-*/   
     public int threeSumMulti(int[] A, int target) {
-        int mod = 1000000007;
-        int ans = 0;
-        Arrays.sort(A);
-        for(int i = 0; i < A.length-2; i++) {
-            long cnt = 0;
-            int l = i+1, r = A.length-1;
-            while(l < r) {
-                if(A[i] + A[l] + A[r] > target) 
-                    r--;
-                else if(A[i] + A[l] + A[r] < target) 
-                    l++;
-                else {
-                    if(A[l] != A[r]) {
-                        long cntL = 1, cntR = 1;
-                        while(l+1 < r && A[l] == A[l+1]) { 
-                            cntL++; l++; 
-                        }
-                        while(l < r-1 && A[r] == A[r-1]) { 
-                            cntR++; r--; 
-                        }
-                        cnt += (cntL * cntR) % mod;
-                        l++;
-                        r--;
-                    } else {
-                        long n = r-l+1;
-                        // If A[l...r] are all equal, then there are combinations of C(n, 2) combinations that meet the requirement.
-                        // let this sink in so we did n*n-1/2   ... n-2! is cancelled out look at the formula.
-                        // nCr = n!/(r!*(n-r)!) 
-                        cnt += (n * (n-1) / 2) % mod;
-                        break;
-                    }
+        HashMap<Integer, Integer> count = new HashMap<>();
+        int n = A.length;
+        long result = 0;
+        
+        for(int i = 0; i + 1 < n; i++){
+            for(int j = i + 1; j < n; j++){
+                if(count.containsKey(target - A[i] - A[j])){
+                    result += ( 1L * count.get(target - A[i] - A[j]) ); 
+                    //System.out.println(result);
                 }
             }
-            ans = (int) (ans + cnt) % mod;
+            count.put(A[i], count.getOrDefault(A[i], 0) + 1);
         }
-        return ans;
+        
+        // System.out.println(result);
+        return (int) (result%(1000000000 + 7));
     }
 }
 
