fd5165706be40e9407441f122d34864c61c439a1
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2020-09-16 01:45:14 -0700

    3SUM < Target, return how many.java

--- 3SUM < Target, return how many.java
+++ 3SUM < Target, return how many.java
@@ -2,18 +2,18 @@
 https://leetcode.com/problems/3sum-smaller/
 Given an integer array A, and an integer target, return the number of tuples i, j, k  
 such that i < j < k and A[i] + A[j] + A[k] < target.
+
+NOTE: 
+You would think sorting will mess up the order of i,j,k indexes 
+This is USELESS detail bcz i,j,k can be reorder to any combination and it doesnt matter.
+A[i] + A[j] + A[k] can be A[k] + A[i] + A[j]
+Moreover there are not even asking to return tuples of i,j,k  
+
 Complexity analysis
 Time complexity : O(n^2). 
-The twoSumSmaller function takes O(n) time because both left and right traverse at most n steps. 
-Therefore, the overall time complexity is O(n^2).
 Space complexity : O(1).
 
 nums = [1, 2, 3, 5, 8], and target = 7.
-
-[1,  2,   3, 5,  8]
- i   left       right 
- target= 7-a[i] = 6 
-
 */
 class Solution{
     public int threeSumSmaller(int[] nums, int target) { 
