9c3b2816c3b400e453108d9167a693ac70d0d3a9
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2020-09-16 02:11:07 -0700

    3SUM = Target, return how many.java

--- 3SUM = Target, return how many.java
+++ 3SUM = Target, return how many.java
@@ -31,4 +31,52 @@ class Solution {
     }
 }
 
-
+/*    Runtime 39 ms. This is same as 3SUM calculating combinations.
+Time complexity: O(N^2)
+Space complexity: O(1)
+NOTE: 
+You would think sorting will mess up the order of i,j,k indexes but it really doesnt matter.
+This is USELESS detail bcz i,j,k can be reorder to any combination and it doesnt matter.
+A[i] + A[j] + A[k] can be A[k] + A[i] + A[j]
+Moreover they are not even asking to return tuples of i,j,k  
+*/   
+    public int threeSumMulti(int[] A, int target) {
+        int mod = 1000000007;
+        int ans = 0;
+        Arrays.sort(A);
+        for(int i = 0; i < A.length-2; i++) {
+            long cnt = 0;
+            int l = i+1, r = A.length-1;
+            while(l < r) {
+                
+                if(A[i] + A[l] + A[r] > target) 
+                    r--;
+                else if(A[i] + A[l] + A[r] < target) 
+                    l++;
+                else {
+                    if(A[l] != A[r]) {
+                        long cntL = 1, cntR = 1;
+                        while(l+1 < r && A[l] == A[l+1]) { 
+                            cntL++; l++; 
+                        }
+                        while(l < r-1 && A[r] == A[r-1]) { 
+                            cntR++; r--; 
+                        }
+                        cnt += (cntL * cntR) % mod;
+                        l++;
+                        r--;
+                    } else {
+                        long n = r-l+1;
+                        // If A[l...r] are all equal, then there are combinations of C(n, 2) combinations that meet the requirement.
+                        // let this sink in so we did n*n-1/2   ... n-2! is cancelled out look at the formula.
+                        // nCr = n!/(r!*(n-r)!) 
+                        cnt += (n * (n-1) / 2) % mod;
+                        break;
+                    }
+                }
+            }
+            ans = (int) (ans + cnt) % mod;
+        }   
+        return ans;
+    }
+}
