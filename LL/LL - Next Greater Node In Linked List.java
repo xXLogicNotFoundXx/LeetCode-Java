e478a67bff0e268eb7a8ef98cfc889fb621e1582
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2020-09-17 22:16:13 -0700

    Update LL - Next Greater Node In Linked List.java

--- LL/LL - Next Greater Node In Linked List.java
+++ LL/LL - Next Greater Node In Linked List.java
@@ -1,6 +1,7 @@
 /*
 https://leetcode.com/problems/next-greater-node-in-linked-list/
-
+    (Similar to Daily Temperatures problem)
+    https://leetcode.com/problems/daily-temperatures/
 Input: 2->1->5
 Output: [5,5,0]
 
@@ -12,11 +13,64 @@ Output: [7,9,9,9,0,5,0,0]
 */
 
 class Solution {
+    
+    // O(N)
     public int[] nextLargerNodes(ListNode head) {
+        Deque<Pair<Integer, Integer>> stack = new LinkedList<>();
+        //Map<Integer, Integer> map = new HashMap<>();
+        int length = 0;
+        ListNode tmp = head;
+        while(tmp!=null){
+            length++;
+            tmp=tmp.next;
+        }
+        int[] result = new int[length];
+        int index = 0;
+        while(head!=null){
+            while(!stack.isEmpty() && stack.peek().getValue() < head.val) {
+                result[stack.pop().getKey()]=head.val;
+            }
+            stack.push(new Pair<Integer, Integer>(index, head.val));
+            head = head.next;
+            index++;
+        }
+        return result;
+    }
+    
+    // O(N)
+    public int[] nextLargerNodes1(ListNode head) {
         if(head==null)
             return new int[0];
         
-        Stack<Integer> stack = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        ArrayList<Integer> aList = new ArrayList<>();
+        while(head!=null){
+            aList.add(head.val);
+        }
+        
+        int[] ans = new int[aList.size()];
+        
+        for(int i=0;i<aList.size();i++){
+            int currentNumber = aList.get(i);
+            
+            while(!stack.isEmpty() && aList.get(stack.peek()) < currentNumber){
+                ans[stack.pop()] = currentNumber;
+            }
+            stack.push(i);
+        }
+        
+        while(!stack.isEmpty())
+            ans[stack.pop()] = 0;
+        
+        return ans;
+    }
+    // Time: (N*Log(N))  (if it is sorted it will be N insertions in minHeap)
+    // Space : N
+    public int[] nextLargerNodes2(ListNode head) {
+        if(head==null)
+            return new int[0];
+        
+        Deque<Integer> stack = new ArrayDeque<>();
         while(head!=null){
             stack.push(head.val);
             head = head.next;
@@ -44,4 +98,6 @@ class Solution {
         
         return ans;
     }
+    
+    
 }
