fbcc620c9f7a7d35be6b72e20596e721623f2738
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2020-09-23 20:47:46 -0700

    Array - Subarray Longest Subarray of 1's After Deleting One Element.java

--- Array - Subarray/Array - Subarray Longest Subarray of 1's After Deleting One Element.java
+++ Array - Subarray/Array - Subarray Longest Subarray of 1's After Deleting One Element.java
@@ -1,6 +1,17 @@
 /*
 1493. Longest Subarray of 1's After Deleting One Element
 https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/
+Given a binary array nums, you should delete one element from it.
+
+Return the size of the longest non-empty subarray containing only 1's in the resulting array.
+Return 0 if there is no such subarray.
+
+Input: nums = [1,1,0,1]
+Output: 3
+
+Input: nums = [0,1,1,1,0,1,1,0,1]
+Output: 5
+
 Time Space : O(N) O(1)
 */
 class Solution {
