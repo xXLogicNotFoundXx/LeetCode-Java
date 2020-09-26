240e84f7291011cced2e5859979fb2e95b909636
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2020-09-25 18:01:48 -0700

    Update Sliding Window Maximum.java

--- Array - Sliding Window/Sliding Window Maximum.java
+++ Array - Sliding Window/Sliding Window Maximum.java
@@ -23,42 +23,42 @@ Using Deque:
 Window position				 Max   Deque
 ---------------				 ---   ——————
  i=0
-[1] 3  -1  -3  5  3  6  7		    -	        1. if first index in Q(deque) < i-k+1 ? no-> so no poll()
-									-			2. loop: nums[i] > last element of the Q(Deque)? no -> so no pollLast()
-									1(0)		3. offer(i)
-							  -		1(0)		4. (i >= k - 1) no -> [window is incomplete].
+[1] 3  -1  -3  5  3  6  7		-	        	1. if first index in Q(deque) < i-k+1 ? no-> so no poll()
+					-			2. loop: nums[i] > last element of the Q(Deque)? no -> so no pollLast()
+						1(0)		3. offer(i)
+					-	1(0)		4. (i >= k - 1) no -> [window is incomplete].
 	i=1								
 [1  3] -1  -3  5  3  6  7			1(0)		1. if first index in Q(deque) < i-k+1 ? no-> so no poll) 
-	   								-			2. loop: nums[i] > last element of the Q(Deque)? yes->so pollLast() -> 1(0) polled
-									3(1)    	3. offer(i)
-							  -		3(1)	    4. (i >= k - 1) no -> [window is incomplete]. 
+	   				-			2. loop: nums[i] > last element of the Q(Deque)? yes->so pollLast() -> 1(0) polled
+						3(1)    	3. offer(i)
+					-	3(1)	    	4. (i >= k - 1) no -> [window is incomplete]. 
 	   i=2
 [1  3  -1] -3  5  3  6  7			3(1)		1. if first index in Q(deque) < i-k+1 ? no-> so no poll() 
-									3(1)		2. loop : nums[i] > last element of the Q(Deque)? no -> so no pollLast()
-									3(1),-1(2)	3. offer (i)	
-							  3		3(1),-1(2)	4. (i >= k - 1) yes -> output q.peek();
+						3(1)		2. loop : nums[i] > last element of the Q(Deque)? no -> so no pollLast()
+						3(1),-1(2)	3. offer (i)	
+					3	3(1),-1(2)	4. (i >= k - 1) yes -> output q.peek();
 		  i=3					    	
- 1 [3  -1  -3] 5  3  6  7 			3(1),-1(2)  1. if first index in Q(deque) < i-k+1 ? no-> so no poll() 
-									3(1)		2. loop: nums[i] > last element of the Q(Deque)? yes -> so pollLast() -> -1(2) polled
-									3(1),-3(3)	3. offer(i)
-							  3		3(1),-3(3)	4. (i >= k - 1) yes -> output q.peek();
+ 1 [3  -1  -3] 5  3  6  7 			3(1),-1(2)  	1. if first index in Q(deque) < i-k+1 ? no-> so no poll() 
+						3(1)		2. loop: nums[i] > last element of the Q(Deque)? yes -> so pollLast() -> -1(2) polled
+						3(1),-3(3)	3. offer(i)
+					3	3(1),-3(3)	4. (i >= k - 1) yes -> output q.peek();
 			i=4
  1  3 [-1  -3  5] 3  6  7			-3(3)   	1. if first index in Q(deque) < i-k+1 ? yes-> so poll() -> 3(1) polled 
-									-			2. loop: nums[i] > last element of the Q(Deque)? yes -> so pollLast() -> -3(3) polled
-									5(4)		3. offer(i)
-							  5		5(4)		4. (i >= k - 1) yes -> output q.peek();
+						-		2. loop: nums[i] > last element of the Q(Deque)? yes -> so pollLast() -> -3(3) polled
+						5(4)		3. offer(i)
+					5	5(4)		4. (i >= k - 1) yes -> output q.peek();
 			     i=5	
  1  3  -1 [-3  5  3] 6  7			5(4)		1. if first index in Q(deque) < i-k+1 ? no-> so no poll() 
-									5(4)		2. loop: nums[i] > last element of the Q(Deque)? no -> so no pollLast()
-									5(4),3(5)	3. offer(i)
-							  5		5(4),3(5)	4. (i >= k - 1) yes -> output q.peek();
+						5(4)		2. loop: nums[i] > last element of the Q(Deque)? no -> so no pollLast()
+						5(4),3(5)	3. offer(i)
+					5	5(4),3(5)	4. (i >= k - 1) yes -> output q.peek();
 			        i=6		
  1  3  -1  -3 [5  3  6] 7			5(4),3(5)	1. if first index in Q(deque) < i-k+1 ? no-> so no poll() 
-									—			2. loop: nums[i] > last element of the Q(Deque)? yes -> so pollLast() -> 3(5) & 5(4) polled
-									6(6)		3. offer(i)
-							  6		6(6)		4. (i >= k - 1) yes -> output q.peek();
+						—		2. loop: nums[i] > last element of the Q(Deque)? yes -> so pollLast() -> 3(5) & 5(4) polled
+						6(6)		3. offer(i)
+					6	6(6)		4. (i >= k - 1) yes -> output q.peek();
 
- 1  3  -1  -3  5 [3  6  7]    7		...
+ 1  3  -1  -3  5 [3  6  7]    		7	...
 
 
 class Solution {
