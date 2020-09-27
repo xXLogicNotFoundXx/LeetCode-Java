439439ebfb690b96b024f190e3aacc973b71a47e
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2020-09-26 23:37:04 -0700

    Update 101 Sliding Window Maximum.java

--- Array - Sliding Window/101 Sliding Window Maximum.java
+++ Array - Sliding Window/101 Sliding Window Maximum.java
@@ -96,3 +96,50 @@ class Solution {
         return ans;
     }
 }
+
+
+
+/*   PRIORITY QUEUE
+if the the array is sorted we will never poll any element from priority queue. 
+O(N*logN)
+*/
+class Solution {
+
+    class Pair{
+        int val;
+        int index;
+        Pair(int val, int index){
+            this.val = val;
+            this.index = index;
+        }
+    }
+    
+    public int[] maxSlidingWindow(int[] nums, int k) {
+       
+        if(nums==null||nums.length==0 ) 
+            return new int[0];
+        
+        PriorityQueue<Pair> maxHeap = new PriorityQueue<Pair>(new Comparator<Pair>(){
+            public int compare(Pair a, Pair b){
+                return b.val-a.val;
+            }
+        });
+        
+        int N = nums.length;
+        int K = k>N ? N:k;
+        
+        int []result = new int[N-K+1];
+        int j=0;
+        
+        for(int i=0;i<nums.length;i++){
+            maxHeap.add(new Pair(nums[i],i));
+            
+            while(maxHeap.size()>0 && i-maxHeap.peek().index>=K)
+                maxHeap.poll();
+            
+            if(maxHeap.size()>=k)
+                result[j++] = maxHeap.peek().val;
+        }
+        return result;
+    }
+}
