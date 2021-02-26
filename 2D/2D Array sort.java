a3b2cf217fe0b947fe4a565374483ce1d945527f
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-02-25 23:40:48 -0800

    Create 2D Array sort.java

---
+++ 2D/2D Array sort.java
@@ -0,0 +1,26 @@
+/*
+https://stackoverflow.com/questions/1066589/iterate-through-a-hashmap/1066603#1066603
+*/
+public static void sortMatrix(final int[][] matrix) {
+    // Assuming the matrix is rectangular
+    final int n = matrix.length;
+    final int m = matrix[0].length;
+
+    List<Integer> list = new AbstractList<Integer>() {
+        @Override
+        public Integer set(int index, Integer element) {
+            return matrix[index/m][index%m] = element;
+        }
+
+        @Override
+        public Integer get(int index) {
+            return matrix[index/m][index%m];
+        }
+
+        @Override
+        public int size() {
+            return n*m;
+        }
+    };
+    Collections.sort(list);
+}
