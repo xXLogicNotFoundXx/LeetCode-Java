f131285f46b5a5759ab32b69e3f416262ad73013
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-02-28 14:14:17 -0800

    Update BT - print binary tree.java

--- BT - print binary tree.java
+++ BT - print binary tree.java
@@ -9,35 +9,47 @@ Output:
  ["2", "", ""]]
 */
 class Solution {
-    
     public List<List<String>> printTree(TreeNode root) {
-        List<List<String>> res = new LinkedList<>();
-
-        int totalRows = root == null ? 1 : getHeight(root);
-        int totalcolumns = (int) (Math.pow(2, totalRows) - 1);
-
+        
+        int height = treeHeight(root);
+        
+        List<List<String>> matrix = new ArrayList<List<String>>();
         List<String> row = new ArrayList<>();
-        for(int i = 0; i < totalcolumns; i++)  
-          row.add("");
-        for(int i = 0; i < totalRows; i++)  
-          res.add(new ArrayList<>(row));
-
-        populateRes(root, res, 0, totalRows, 0, totalcolumns - 1);
-        return res;
+        
+        int maxCol = (int) Math.pow(2,height)-1; // total number of nodes in fully BT is (2^Height)-1
+        
+        for(int i=0; i<maxCol; i++)
+            row.add("");
+        
+        for(int i=0; i<height; i++)
+            matrix.add(new ArrayList<String>(row));
+        
+        
+        traverse(root, 0/*H*/, 0, maxCol-1, matrix); // -1 because we want it inclusive of the indexes. 
+        
+        return matrix;
     }
-
-    public void populateRes(TreeNode root, List<List<String>> res, int row, int totalRows, int i, int j) {
-        if (row == totalRows || root == null) 
-            return;
-
-        res.get(row).set((i+j)/2, Integer.toString(root.val));
-
-        populateRes(root.left, res, row+1, totalRows, i, (i+j)/2 - 1);
-        populateRes(root.right, res, row+1, totalRows, (i+j)/2+1, j);
+    
+    void traverse(TreeNode root, int height, int i, int j, List<List<String>> matrix){
+        if(root==null)
+            return; 
+        
+        int mid = (i+j)/2;
+        List<String> row = matrix.get(height);
+        
+        row.set(mid,""+root.val); // int to string ?  there is no new String(int)
+        
+        traverse(root.left, height+1, i, mid-1,  matrix);
+        traverse(root.right, height+1, mid+1, j,  matrix);
     }
-
-    public int getHeight(TreeNode root) {
-         if (root == null) return 0;
-         return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+    
+    int treeHeight(TreeNode root){
+        if(root==null)
+            return 0;
+        
+        int Rhgt = treeHeight(root.right);
+        int Lhgt = treeHeight(root.left);
+        
+        return Math.max(Rhgt,Lhgt) + 1;
     }
 }
