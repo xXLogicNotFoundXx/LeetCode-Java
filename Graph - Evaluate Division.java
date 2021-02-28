c20dd4a1ce439c64d2ee47eebe4b89a2a998d2ec
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-02-28 13:59:05 -0800

    Update Graph - Evaluate Division.java

--- Graph - Evaluate Division.java
+++ Graph - Evaluate Division.java
@@ -17,6 +17,70 @@ b: [a:0.5], [c:3.0]
 c: [b:0.333]]
 */
 class Solution {
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        
+        Map<String, Map<String,Double>> map = new HashMap<>();
+        buildEquations(map, equations, values);
+
+        double []res = new double[queries.size()];
+        
+        int i=0;
+        for(List<String> query : queries){
+            Double dRes = calcEquationHelper(map, new HashSet<String>(), query.get(0), query.get(1));    
+            res[i++] = dRes==null ? -1 : dRes;
+        }
+        return res;
+    }
+    
+    // Here we learned that if we want invalid value if primitive type int, double
+    // use Object and use null as invalid or else it is a valid value. 
+    Double calcEquationHelper( Map<String, Map<String,Double>> map, HashSet<String> visited, String var1, String var2){
+        
+        if(!map.containsKey(var1) || !map.containsKey(var2)) 
+            return null;
+        
+        if(visited.contains(var1)) 
+            return null;
+        
+        if(var1.equals(var2)) return 1.0;   // IMP 
+        
+        visited.add(var1);
+        if(map.containsKey(var1)){
+            
+            // if direct path 
+            if( map.get(var1).containsKey(var2) ) 
+                return map.get(var1).get(var2);
+            
+            for(Map.Entry<String,Double> e : map.get(var1).entrySet()){
+                
+                Double r = calcEquationHelper(map, visited, e.getKey(), var2);
+                if(r!=null)
+                    return r * e.getValue();
+            }
+        }
+        visited.remove(var1);
+        return null;
+    }
+
+    void buildEquations(Map<String, Map<String,Double>> map, List<List<String>> equations, double[] values){
+        
+        for(int i=0;i<equations.size();i++) {
+            
+            List<String> eq = equations.get(i);
+            
+            String var1=eq.get(0);
+            String var2=eq.get(1);
+            double res=values[i];
+            
+            map.putIfAbsent(var1,new HashMap<String,Double>());
+            map.putIfAbsent(var2,new HashMap<String,Double>()); 
+            
+            map.get(var1).put(var2,res);
+            map.get(var2).put(var1,1.0/res);
+        }
+    }
+    /*
     class DenomResult {
         String den;
         double res; 
@@ -68,5 +132,5 @@ class Solution {
         map.get(var1).add(new DenomResult(var2,res));
         map.get(var2).add(new DenomResult(var1,1.0/res));
     }
-    
+    */
 }
