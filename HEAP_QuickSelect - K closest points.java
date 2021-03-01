5cafc40324d52c4f18815eefb5766ba8d4b960df
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-02-28 16:17:23 -0800

    Update HEAP_QuickSelect - K closest points.java

--- HEAP_QuickSelect - K closest points.java
+++ HEAP_QuickSelect - K closest points.java
@@ -1,39 +1,49 @@
-// https://leetcode.com/problems/k-closest-points-to-origin/
-class Solution {
-    /* 1 NLogN sort the array and return K 
-    public int[][] kClosest(int[][] points, int K) {
-        Arrays.sort(points, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] p1, int[] p2) {
-                return p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1];
-            }
-        });
-        return Arrays.copyOfRange(points, 0, K);
-    }
-    */
-    // 2 
-    // we can use MAX_HEAP and keep K items in the heap at anygiven point
-    // Theoretically, the time complexity is O(NlogK), 24ms
-    // The advantage of this solution is it can deal with real-time(online) stream data. It does not have to know the size of the data previously.
+/* https://leetcode.com/problems/k-closest-points-to-origin/
 
-    class PointDistance implements Comparator<PointDistance>{
-        int x,y,distance;
-        PointDistance(int x, int y){
+We have a list of points on the plane.  Find the K closest points to the origin (0, 0).
+(Here, the distance between two points on a plane is the Euclidean distance.)
+You may return the answer in any order.  The answer is guaranteed to be unique (except for the order that it is in.)
+
+
+distance between (x1,y1) & (x2,y2) is =  sqrt( (x1-x2)^2 + (y1-y2)^2) 
+in this case one point is 0,0 => sqrt(x2^2 + y2^2)
+we dont need the actual distance so we can just say x^2 + y^2 is our distance 
+Sort or put in priority queue. 
+
+1 NLogN sort the array and return K 
+public int[][] kClosest(int[][] points, int K) {
+    Arrays.sort(points, new Comparator<int[]>() {
+        @Override
+        public int compare(int[] p1, int[] p2) {
+            return p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1];
+                        //   X^2 + Y^2           -   A^2 + B^2 
+        }
+    });
+    return Arrays.copyOfRange(points, 0, K);
+}
+
+ 2 
+ we can use MAX_HEAP and keep K items in the heap at anygiven point
+ Theoretically, the time complexity is O(NlogK), 24ms
+ The advantage of this solution is it can deal with real-time(online) stream data. It does not have to know the size of the data previously.
+*/
+class Solution {
+    class PointDistance {
+        public int x,y,distance;
+        public PointDistance(int x, int y){
             this.x = x;
             this.y = y;
             this.distance = x*x + y*y;
         }
-        PointDistance(){}
-        public int compare(PointDistance a, PointDistance b){
-            return b.distance - a.distance;
-        }
     }
     
-    public int[][] kClosest1(int[][] points, int K) {
+    public int[][] kClosest(int[][] points, int K) {
         if(points == null || points.length <=K) 
             return points;
         
-        PriorityQueue<PointDistance> queue = new PriorityQueue<PointDistance>(new PointDistance());
+        // MAX HEAP
+        PriorityQueue<PointDistance> queue = new PriorityQueue<PointDistance>((a,b) -> b.distance - a.distance);
+        
         for(int i=0;i<points.length;i++){
             queue.add(new PointDistance(points[i][0],points[i][1]));
             if(queue.size()>K)
@@ -50,14 +60,15 @@ class Solution {
         return ans;
     
     }
+    
     /*
     III. The last solution is based on quick sort, we can also call it quick select. 
     In the quick sort, we will always choose a pivot to compare with other elements. 
     After one iteration, we will get an array that all elements smaller than the pivot are on the left side of the pivot
     and all elements greater than the pivot are on the right side of the pviot (assuming we sort the array in ascending order). 
-    average time complexity O(N) - 5ms 
+    O(N) 5ms 
     */
-    public int[][] kClosest(int[][] points, int K) {
+    public int[][] kClosest1(int[][] points, int K) {
         if(points == null || points.length <=K) 
             return points;
         
