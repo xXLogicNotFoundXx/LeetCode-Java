d8f7f23563ef66ff12d307439f8f2243f8c3c4a8
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-02-28 17:43:17 -0800

    Update 101 - sqrt(n).java

--- Binary Search/101 - sqrt(n).java
+++ Binary Search/101 - sqrt(n).java
@@ -9,9 +9,25 @@ Compute and return the square root of x, where x is guaranteed to be a non-negat
 9 => 3
 */
 class Solution {
-    // think of 81  = 9
-    // think of 101 = 10 
-    // think of 110 = 10
+    /* 
+        we want the next lowest(Floor value) or equal ....
+        so we should NOT move L,  L=mid because that "L" could be our ans 
+        & r should be r=mid-1 
+        
+        
+        Rule of thumb     
+        m = r - (r-l)/2                     m = l + (r-l)/2 
+        with                                with
+        l = m                               l = m+1
+        r = m - 1                           r = m
+        
+        (Finding Floor Value or equal)      (Finding Ceiling Value or equal)
+        
+        This can prevent m from getting stuck at r (or l) after the updating step.
+        
+        We want floor value and left could be answer.
+    */
+    
     public int mySqrt(int x) {
         if (x == 0)
             return 0;
@@ -19,23 +35,19 @@ class Solution {
         int left = 1, right = x;
         int ans =0;
         
-        while (left<=right) {
-            int mid = left + (right - left)/2;
+        while (left<right) {
+            int mid = right - (right - left)/2;
             
             int div =  x/mid;
-            
-            if(mid == div){
-                return mid;
-            }
-            
+
             if (div < mid) { // 81/10 = 8 which is less than mid 
                 right = mid - 1;
-            } else {           // 81/8 = 10 which is greater than mid  so it could be ans
-                ans = mid;
-                left = mid + 1;
+            } else {          
+                left = mid ;
             }
         }
         
-        return ans;
+        return left;
     }
+    
 }
