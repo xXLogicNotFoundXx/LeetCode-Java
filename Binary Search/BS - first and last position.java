fde097e71b897fc1edbf71edff1bf5210e5fa176
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-03-03 14:21:44 -0800

    Update BS - first and last position.java

--- Binary Search/BS - first and last position.java
+++ Binary Search/BS - first and last position.java
@@ -6,27 +6,57 @@ Output: [3,4]
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int []ans = new int[2];
+            
+        if(nums.length==0){
+            return new int[]{-1,-1};
+        }
+        
+        ans[0] = findLeftIndex(nums,target);
+        ans[1] = findRightIndex(nums,target);
+        
+        return ans;
+    }
+    
+    int findLeftIndex(int[] nums, int target){
         int left = 0, right = nums.length-1;
-        ans[0] = ans[1] = -1;
-        while(left<=right){
+        int res = -1;
+        while(left<=right)
+        {
             int mid = left + (right-left)/2;
-            if(nums[mid] < target){
-                left=mid+1;
-            }else if(nums[mid] > target){
-                right=mid-1;
-            }else if(nums[mid]==target){
-                if(nums[left] == nums[mid] && nums[mid] == nums[right]){
-                    ans[0] = left;
-                    ans[1] = right;
-                    break;
-                } 
-                if(nums[left] < nums[mid])
-                    left++;
-                if(nums[right] > nums[mid])
-                    right--;
+            
+            if(nums[mid]==target){
+                res = mid;
+                right = mid-1;
+            }
+            else if(nums[mid]>target){
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
             }
         }
-        return ans;
+        return res;
+    }
+    
+    int findRightIndex(int[] nums, int target){
+        int left = 0, right = nums.length-1;
+        int res = -1;
+        while(left<=right)
+        {
+            int mid = left + (right-left)/2;
+            
+            if(nums[mid]==target){
+                res = mid;
+                left = left+1;
+            }
+            else if(nums[mid]>target){
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return res;
     }
 }
 
