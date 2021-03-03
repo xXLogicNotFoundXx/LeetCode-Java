72873f5b79fced4d148b7c35dab50d0f1db2b94a
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-03-02 19:03:11 -0800

    Update Graph - Clone Graph.java

--- Graph - Clone Graph.java
+++ Graph - Clone Graph.java
@@ -4,33 +4,77 @@ Given a reference of a node in a connected undirected graph, return a deep copy 
 Each node in the graph contains a val (int) and a list (List[Node]) of its neighbors.
 1--2
 |  |
-3--4
+4--3
 Node 1 has two neighbors: Node 2 and 4.           class Node {
 Node 2  has two neighbors: Node 1 and 3.            public int val;
 Node 3 has two neighbors: Node 2 and 4.             public List<Node> neighbors;
-Node 3 has two neighbors: Node 2 and 4              public Node(int _val,List<Node> _neighbors) {
+Node 3 has two neighbors: Node 2 and 4              public Node(int _val, List<Node> _neighbors) {
 Node 4 has two neighbors: Node 1 and 3.               val = _val;
                                                       neighbors = _neighbors;
                                                     }
+                                                    public Node(int _val) {
+                                                        val = _val;
+                                                        neighbors = new ArrayList<Node>();
+                                                    }
                                                   }
 */
+
 class Solution {
-    public Node cloneGraph(Node node) {
-        if(node == null) return null;
-        Map<Integer,Node> map = new HashMap<>();
-        cloneGraphHelper(node,map);
-        return map.get(node.val);
+    /*  
+        2Pass - 25ms 
+        O(E+V)
+        Do we need the second loop though? 
+    */
+    public Node cloneGraph1(Node node) {
+        Map<Node,Node> map = new HashMap<>();
+        
+        traverse(node, map);
+        for(Map.Entry<Node,Node> e: map.entrySet()){
+            Node root = e.getKey();
+            Node newRoot = e.getValue();
+            
+            for(Node child : root.neighbors){
+                newRoot.neighbors.add(map.get(child));
+            }
+        }
+        
+        return map.get(node);
     }
     
-    void cloneGraphHelper(Node n, Map<Integer,Node> map) {
-        if(n==null) return; 
-        if(map.containsKey(n.val)) return; 
+    void traverse(Node root, Map<Node,Node> map){
+        if(root==null || map.containsKey(root))
+            return; 
         
-        Node n1 = new Node(n.val, new ArrayList<Node>());
-        map.put(n.val,n1);
-        for(Node ch : n.neighbors){
-            cloneGraphHelper(ch,map);
-            n1.neighbors.add(map.get(ch.val));
+        Node newRoot = new Node(root.val);
+        map.put(root,newRoot);
+        
+        for(Node child : root.neighbors){
+            traverse(child, map);
         }
+    }
+    
+    // 1 Pass we dont have to iterate over map.entries 
+    public Node cloneGraph(Node node) {
+        Map<Node,Node> map = new HashMap<>();
+        Node newRoot = traverse2(node,map);
+        
+        return newRoot;
+    }
+    
+    Node traverse2(Node root, Map<Node,Node> map){
+        if(root==null)
+            return null; // should not happen 
+        
+        if(map.containsKey(root))
+            return map.get(root);
+        
+        Node newRoot = new Node(root.val);
+        map.put(root,newRoot);
+        
+        for(Node child : root.neighbors){
+            newRoot.neighbors.add(traverse2(child, map));
+        }
+        
+        return newRoot;
     }
 }
