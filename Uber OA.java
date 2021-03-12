6bdd126277adafc19e297a98f7e2e0f94859f086
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-03-11 23:34:18 -0800

    Update Uber OA.java

--- Uber OA.java
+++ Uber OA.java
@@ -316,8 +316,8 @@ for(Integer i: list) {
 	map.remove(i);
 }
 
-	   /*
-Group array by mean values 
+/*
+Group array by mean values:
 */
 private static int[][] meanGroups(int[][] nums) {
         
@@ -352,3 +352,96 @@ private static int[][] meanGroups(int[][] nums) {
         return sum/A.length;
     }
 
+	   
+	   
+	   
+	   
+/*
+delete min of peak elements one by one in an array
+Peak element defined as it should be greater than it’s neighbors.
+2,7,8,5,1,6,3,9,4
+
+8,6,9 are peak elements. You should remove 6 first.
+
+Ans: 6,8,7,5,9,2,4,3,1
+https://www.geeksforgeeks.org/minimum-peak-elements-from-an-array-by-their-repeated-removal-at-every-iteration-of-the-array/
+*/
+	   
+ void minPeaks(ArrayList<Integer> list){
+ 
+        // Length of original list
+        int n = list.size();
+ 
+        // Initialize resultant list
+        ArrayList<Integer> result = new ArrayList<>();
+ 
+        // Traverse each element of list
+        for (int i = 0; i < n; i++) {
+ 
+            int min = Integer.MAX_VALUE;
+            int index = -1;
+ 
+            // Length of original list after
+            // removing the peak element
+            int size = list.size();
+ 
+            // Traverse new list after removal
+            // of previous min peak element
+            for (int j = 0; j < size; j++) {
+ 
+                // Update min and index,
+                // if first element of
+                // list > next element
+                if (j == 0 && j + 1 < size) {
+ 
+                    if (list.get(j) > list.get(j + 1)
+                        && min > list.get(j)) {
+                        min = list.get(j);
+                        index = j;
+                    }
+                }
+ 
+                else if (j == size - 1 && j - 1 >= 0) {
+ 
+                    // Update min and index,
+                    // if last elemnt of
+                    // list > previous one
+                    if (list.get(j) > list.get(j - 1)
+                        && min > list.get(j)) {
+                        min = list.get(j);
+                        index = j;
+                    }
+                }
+ 
+                // Update min and index, if
+                // list has single element
+                else if (size == 1) {
+ 
+                    min = list.get(j);
+                    index = j;
+                }
+ 
+                // Update min and index,
+                // if current element >
+                // adjacent elements
+                else if (list.get(j) > list.get(j - 1)
+                         && list.get(j) > list.get(j + 1)
+                         && min > list.get(j)) {
+ 
+                    min = list.get(j);
+                    index = j;
+                }
+            }
+ 
+            // Remove current min peak
+            // element from list
+            list.remove(index);
+ 
+            // Insert min peak into
+            // resultant list
+            result.add(min);
+        }
+ 
+        // Print resultant list
+        System.out.println(result);
+    }
