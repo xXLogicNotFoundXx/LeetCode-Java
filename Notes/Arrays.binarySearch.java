0891d0440124603c81bdb20dd4fbdb62d3d7306f
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-03-11 23:52:05 -0800

    Update Arrays.binarySearch.java

--- Notes/Arrays.binarySearch.java
+++ Notes/Arrays.binarySearch.java
@@ -1,6 +1,7 @@
     /*
 Arrays.binarySearch Returns:
-index of the search key, if it is contained in the array; otherwise, (-(insertion point) - 1). The insertion point is defined as the point at which the key would be inserted into the array: the index of the first element greater than the key, or a.length if all elements in the array are less than the specified key. Note that this guarantees that the return value will be >= 0 if and only if the key is found.
+index of the search key, if it is contained in the array; otherwise, (-(insertion point) - 1). 
+Note that this guarantees that the return value will be >= 0 if and only if the key is found.
     */
     // Driver Code
     public static void main(String[] args)
