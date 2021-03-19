6f85d2427890090d9d7d688033b3d3cc1953170b
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-03-18 21:11:43 -0700

    Update DP - 2D Maximal square.java

--- DP - 2D Maximal square.java
+++ DP - 2D Maximal square.java
@@ -8,69 +8,66 @@ Given a 2D binary matrix filled with 0's and 1's, find the largest SQUARE contai
 Output: 4
 */
 class Solution {
-    
-     // DP APPROACH 
+       
+    // DP APPROACH 
     // it same as edit distance problem.
     // dp[i][j] = Math.min(Math.min([i-1][j],[i][j-1]), [i-1][j-1]) 
     // think about this and let that sink in.
-    public int maximalSquare(char[][] matrix) {
+    
+    public int maximalSquare1(char[][] matrix) {
         if(matrix==null || matrix.length ==0 || matrix[0].length ==0 )
             return 0;
         int [][]dp = new int[matrix.length+1][matrix[0].length+1];
         int maxLength = 0;
+        
         for(int i=1;i<dp.length;i++){
             for(int j=1;j<dp[0].length;j++){
                 if(matrix[i-1][j-1]=='1'){
-                    dp[i][j] = Math.min(Math.min(dp[i-1][j],dp[i][j-1]), dp[i-1][j-1])+1;
+                    
+                    int min = Math.min(dp[i-1][j], dp[i][j-1]); // left and up 
+                    min = Math.min(min, dp[i-1][j-1]);  // top left 
+                                                
+                    dp[i][j] = min+1;
                     maxLength = Math.max(maxLength, dp[i][j]);
                 }
             }
         }
+        
         return maxLength*maxLength; 
     }
     
-    
-    // very simeple everytime you find 1 expand and note down the max ... messed up lot of indexes 
-    // though looks simple but be careful about manipulating indexes 
-    // (mn)^2 
-    public int maximalSquare1(char[][] matrix) {
-        if(matrix==null || matrix.length ==0 || matrix[0].length ==0 )
-            return 0;
-        int rows = matrix.length;
-        int cols = matrix[0].length; 
-        int maxLength = 0;
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(matrix[i][j]=='1'){
-                    int length = 1;
-                    boolean flag = true;
-                    while(flag && i+length<rows && j+length<cols){
-                        // check all colums of a last row 
-                        for(int k = j; k <= j+length; k++){ // IMP index calculation 
-                            if(matrix[i+length][k]!='1'){
-                                flag=false;
-                                break;
-                            }
-                        }
-                        // check all row of a last column 
-                        for(int k = i; k <= i+length; k++){ // IMP index calculation 
-                            if(matrix[k][j+length]!='1'){
-                                flag=false;
-                                break;
-                            }
-                        }
-                        
-                        if(flag)
-                            length++;
-                    }
-                    maxLength = Math.max(maxLength,length);    
+    // This is better .... easy to understand ... 
+    // fuckers did the character[] otherwise you dont even need dp[][]
+    // [["0","1"],     need to handle this case ...
+    //  ["1","0"]]
+    public int maximalSquare(char[][] matrix) {
+        
+        int[][] intMat = new int[matrix.length][matrix[0].length];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                intMat[i][j] = matrix[i][j] == '1' ? 1 : 0;
+            }
+        }
+        
+        int maxLength=0;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                
+                if(i==0 || j==0){
+                    maxLength = Math.max(maxLength, intMat[i][j]); // if we have 1 in only top row or first column ans should be 1 
+                }
+                else if(intMat[i][j]==1){
+                    
+                    int min = Math.min(intMat[i-1][j], intMat[i][j-1]); // left and up 
+                    min = Math.min(min, intMat[i-1][j-1]); // top left 
+                    
+                    intMat[i][j] = min+1;
+                    maxLength = Math.max(maxLength, intMat[i][j]);
                 }
             }
         }
+         
         return maxLength*maxLength;
-    }
-    
-    
-    
-   
+     }
+                                                
 }
