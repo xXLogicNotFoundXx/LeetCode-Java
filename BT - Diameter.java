1944aaee69dae4cd732ccd510c008b1e56a6f14a
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-03-28 00:12:06 -0700

    Update BT - Diameter.java

--- BT - Diameter.java
+++ BT - Diameter.java
@@ -35,3 +35,34 @@ class Solution {
         return Math.max(left,right)+1; 
     }
 }
+
+// N-ary tree 
+class Solution {
+    int res = 0;
+    public int diameter(Node root) {
+        getHeight(root);
+        return res;
+    }
+    
+    public int getHeight(Node root) {
+        if(root == null)
+            return 0;
+        
+        int max1 = 0;
+        int max2 = 0;
+              
+        for(Node child : root.children) {
+            int height = getHeight(child);
+            if(height > max1) {
+                max2 = max1;
+                max1 = height;
+            }
+            else if(height > max2) {
+                max2 = height;
+            }
+        }
+        
+        res = Math.max(res, max1+max2);
+        return max1+1;
+    }
+}
