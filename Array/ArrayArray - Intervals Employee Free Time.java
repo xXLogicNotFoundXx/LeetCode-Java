84a77fff024c2fad058a1873d6763a6be8dd73b5
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-08-03 23:18:50 -0700

    ArrayArray - Intervals Employee Free Time.java

--- Array/ArrayArray - Intervals Employee Free Time.java
+++ Array/ArrayArray - Intervals Employee Free Time.java
@@ -1,4 +1,6 @@
 /*
+Hard - IMP
+
 https://leetcode.com/problems/employee-free-time/
 
 We are given a list schedule of employees, which represents the working time for each employee.
@@ -8,18 +10,30 @@ Return the list of finite intervals representing common, positive-length free ti
 For example, schedule[0][0].start = 1, schedule[0][0].end = 2, and schedule[0][0][0] is not defined).  
 Also, we wouldn't include intervals like [5, 5] in our answer, as they have zero length.
 
-xample 1:
+example 1:
 
 Input: schedule = [ [[1,2],[5,6]], [[1,3]], [[4,10]]]
 Output: [[3,4]]
 Explanation: There are a total of three employees, and all common
 free time intervals would be [-inf, 1], [3, 4], [10, inf].
 We discard any intervals that contain inf as they aren't finite.
-Example 2:
 
+Example 2:
 Input: schedule = [ [[1,3],[6,7]], [[2,4]], [[2,5],[9,12]]]
 Output: [[5,6],[7,9]]
 
+*/
+
+/*
+
+1 Sort all events in start time then according to end time. 
+2 Initiate start and end with first interval 
+    
+3 if end is greter than nextStart ( we have to expand the interval of working employee )
+        end becomes = Math.max(end, nextEnd); 
+
+4 if end is less than nextStart ( we have found the free time )
+        freeTime = [end, nextStart]
 */
 
 class Solution {
@@ -36,24 +50,34 @@ class Solution {
             }
         }
         
-        // sort by start and then end time 
+        // Sort all events in start time then according to end time. 
         Collections.sort(list, (a,b) -> a.start==b.start ? a.end-b.end : a.start-b.start);
+        // OR
+        Collections.sort(list, new Comparator<Interval>(){
+            public int compare(Interval a, Interval b){
+                if(a.start==b.start)
+                    return a.end - b.end;
+                
+                return a.start - b.start;
+            }
+        });
+        
         int start = list.get(0).start;
         int end = list.get(0).end;
         
-        // expand interval .. if cant expand the gap is free time 
         for(int i=1; i<list.size(); i++){
-            Interval intr = list.get(i);
+            Interval next = list.get(i);
             
-            if(intr.start <= end) {
+            // if
+            if(next.start <= end) {
                 // start is beetwing prev interval
-                end = Math.max(intr.end, end);
+                end = Math.max(end, next.end);
             } else {
-                Interval freeTime = new Interval(end,intr.start);
+                Interval freeTime = new Interval(end, next.start);
                 ans.add(freeTime);
                 
-                start = intr.start;
-                end =   intr.end;
+                start = next.start;
+                end =   next.end;
             }
         }
         
