bd515dc19ec5bf4ed2bb6b08f6ca280c32a46425
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-08-16 14:33:50 -0700

    Backtracking - 101 Subset.java

--- Backtracking/Backtracking - 101 Subset.java
+++ Backtracking/Backtracking - 101 Subset.java
@@ -1,13 +1,17 @@
 /*
 https://leetcode.com/problems/subsets/
+
 Given a set of distinct integers, nums, return all possible subsets (the power set).
 Note: The solution set must not contain duplicate subsets.
-Example:
+
 Input: nums = [1,2,3]
 Output:
 [ [], [1], [1,2], [1,2,3], [1,3], [2], [2,3], [3] ]
-Runtime : O(n*2^n) . A set with n elements has 2^n subsets and max elements in a subset is n.
-Space : It is also O(n*2^n). we store all the subsets.
+
+A set with n elements has 2^n subsets and max elements in a subset is n.
+
+Time : O(n*2^n)  =  generate all subsets and then copy them into output list
+Space : It is also O(n*2^n) = we store all the subsets.
 */
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
