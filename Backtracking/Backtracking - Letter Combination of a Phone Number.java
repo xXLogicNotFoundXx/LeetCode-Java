9502d2e9e00400b8885f9ea0b5fd7ce0ac5a2040
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-08-16 19:03:25 -0700

    Backtracking - Letter Combination of a Phone Number.java

--- Backtracking/Backtracking - Letter Combination of a Phone Number.java
+++ Backtracking/Backtracking - Letter Combination of a Phone Number.java
@@ -5,30 +5,41 @@ A mapping of digit to letters is just like on old telephone buttons.
 
 Input: "23"
 Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+VIMP 
+There are 4 path maximum(4 chars/choices) for a given digit. 
+So it is gonna be 4^* and N is the length of digits. So : 
+Time complexity: O(4^N x N ) - N space for each answer. 
+Space is O(N) that is the height of the call tree. N is the length.
+
 */
 class Solution {
     //  Time and space complexity = O(3^N * 4^M)
     public List<String> letterCombinations(String digits) {
         String[] mapping = new String[] {" ", "*", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}; 
+        
         // LeetCode needed mapping 0->" " and 1->"*" you can ask whats the mapping they want
         if(digits==null || digits.length()==0) 
             return new ArrayList();
+        
         List<String> ans = new ArrayList();
         StringBuffer oneAns = new StringBuffer();
         letterCombinationsHelper(digits,mapping,0,oneAns,ans);
         return ans;
     }
     
-    void letterCombinationsHelper(String digits,String[] mapping, int i, StringBuffer oneAns, List<String> ans){
+    void letterCombinationsHelper(String digits, String[] mapping, int i, StringBuffer oneAns, List<String> ans){
         if(i==digits.length()){
             ans.add(oneAns.toString());
             return;
         } 
-        int mapI = Integer.valueOf(digits.charAt(i)-'0'); // here minus of '0' is very important i forgot that one 
+        
+        int mapI = Integer.valueOf(digits.charAt(i) - '0'); // minus ascii value of zero 
+        
         for(char ch : mapping[mapI].toCharArray()){
             oneAns.append(ch);
             letterCombinationsHelper(digits,mapping,i+1,oneAns,ans);
-            oneAns.setLength(oneAns.length()-1);
+            oneAns.setLength(oneAns.length()-1); // It's O(1) if the new length is less than the old one
         }
     }
 }
