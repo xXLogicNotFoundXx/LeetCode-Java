913d87a1f87363fbbf76084819376d415fdb5f46
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-08-16 22:15:49 -0700

    Backtracking - N-Queens.java

--- Backtracking/Backtracking - N-Queens.java
+++ Backtracking/Backtracking - N-Queens.java
@@ -13,11 +13,24 @@ Output:
 
 
 Time Complexity: 
-F(N) = N (queen placement) * F(N-1) 
+F(N) = Nth (queen placement) * F(N-1) 
 O(N!)
 
 Space : O(N) the call stack is max N times. 
 
+The MAIN problem to solve is.. how do you decide that placing one queen at particulat position is ok?
+and no queens are attacking each other.
+
+Once you answer that, then other part you can do by using recursion. If this poisiton is ok then go to next row.
+While building one row answer at a time. 
+
+How do you decide the position is safe very fast? 
+ We can have column[N[ boolean array and once we put a queen in one position. That column is takes and next rows cant take that column for queens.
+ Similarly we can take diagonal[(N+N)-1] boolean array, do the calculation to find unique index for all diagonal elements.
+   For any x,y we can generate unique index by doing x+y.
+ Similarly we can take AntiDiagonal[(N+N)-1] boolean array 
+   For any xy, we can genrate unique index by x-y+N ... here N has to be y's max value ( +N so that -> index value never goes below 0 ( N and y canceles out)
+                                           OR y-x+M ... here M has to be x's max value ( +M so that -> index value never goes below 0 ( M and x canceles out)
 */
 class Solution {
     public List<List<String>> solveNQueens(int n) {
@@ -27,8 +40,8 @@ class Solution {
             return finalRes;
         
         boolean[] col = new boolean[n];
-        boolean[] diag = new boolean[n*2-1];
-        boolean[] antiDiag = new boolean[n*2-1];
+        boolean[] diag = new boolean[(n+n)-1];
+        boolean[] antiDiag = new boolean[(n+n)-1];
         
         nQueen(finalRes,new ArrayList<String>(), n, 0, col, diag, antiDiag);
         return finalRes;
@@ -44,12 +57,12 @@ class Solution {
         
         StringBuilder str = new StringBuilder(new String(".").repeat(size)); // <- Nice!
         for(int c=0;c<size;c++){
-            if(col[c] || diag[row + c] || antiDiag[row - c + size - 1]) // <- Remember this calculations.
+            if(col[c] || diag[row + c] || antiDiag[row - c + size - 1]) // <- this calculations for the anti-diagonal ...
                 continue; 
             
             col[c] = true;
             diag[row + c] = true;
-            antiDiag[row - c + size - 1] = true;
+            antiDiag[row - c + size - 1] = true; // <- this calculations for the anti-diagonal....
             str.setCharAt(c,'Q');
             oneResult.add(str.toString());
             
