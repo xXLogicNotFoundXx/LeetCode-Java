9da14f7c4846c65d7b83c4b1b07c23f2042490a9
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-08-21 12:24:42 -0700

    Update DP - Maximal Histogram Rectangle.java

--- DP/DP - Maximal Histogram Rectangle.java
+++ DP/DP - Maximal Histogram Rectangle.java
@@ -1,4 +1,7 @@
 /*
+Hard 
+All companies 2-3 times at least. 
+
 https://leetcode.com/problems/largest-rectangle-in-histogram/
 Given an array of integers heights representing the histogram's bar height where the width of each bar is 1,
 return the area of the largest rectangle in the histogram.
