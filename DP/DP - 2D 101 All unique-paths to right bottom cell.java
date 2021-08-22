3a458fe5298302140afe8115bbc32343273bbca3
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-08-21 20:01:20 -0700

    Update DP - 2D 101 All unique-paths to right bottom cell.java

--- DP/DP - 2D 101 All unique-paths to right bottom cell.java
+++ DP/DP - 2D 101 All unique-paths to right bottom cell.java
@@ -85,10 +85,25 @@ This is also tricky question. You have to realise :
 2 You also need to realise
 	1 if there is an obstactle in the first row .. all subsequent positions are gonna be obstacle.
 	2 if there is an obstactle in the first column .. all subsequent column positions are gonna be obstacle.
+
+
+
+	******** Lesson Learnt *********
+	Usually I go and try to define a state with '-ve' number OR some INT_MAX constant. 
+		- This is almost all the time ends up being messy and a lot state checking
+			- results into not solving the problem at all or taking too much time. 
+		- Ends up being lot of if and elses 
+		
+	Avoid it .... 
+	come up with the algorithm .... and 
+		- try zero value would that work with genral algorithm?
+			- yes then try some corner cases
+		- 
 */
 
 /* 
-	We do need first loops. Incorporating logic in one i,j loop is not possible (lots of ifs and buts) 
+	We do need first three loops. Incorporating logic in one i,j loop is not possible (lots of ifs and buts) 
+	
 	******** Lesson learnt ********* 
 	If you think extra for loops can make your life easy, do it. 
 	Dont try to get that logic in the main logic.
