c041a66255e2235a9a2eb56b872931e6675c6a03
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-09-15 20:41:46 -0700

    Update DP - xMEMO Regular Expression Matching.java

--- DP - xMEMO Regular Expression Matching.java
+++ DP - xMEMO Regular Expression Matching.java
@@ -13,6 +13,7 @@ Output: true        Output: true
 class Solution {
     
     public boolean isMatch(String s, String p) {
+        
         // p starting with * or containing ** is not correct pattern so false 
         if(p!=null && !p.isEmpty() && (p.charAt(0)=='*' || p.contains("**")))
             return false; 
@@ -30,22 +31,37 @@ class Solution {
             return dp[i][j];
         
         Boolean ans = new Boolean("false");
+        
+        // We need to look for * in the next index 
+        // So that we know what character to match for the *
+        // if (j+1) is valid index and j+1 is '*'
         if(j+1 < p.length() && p.charAt(j+1)=='*'){
+            
             // zero match 
             ans = isMatchHelper(s,p,dp,i,j+2);
+            
+            // if zero match did not work out then match one character 
             if(ans==false){
-                // one match or it is false
+                // if it is one match else it is false 
                 if(i < s.length() && (s.charAt(i)==p.charAt(j) || p.charAt(j)=='.'))
                     ans = isMatchHelper(s,p,dp,i+1,j);
+                else
+                    ans = false;
             }
             
         } else {
             // one match or it is false
             if(i < s.length() && (s.charAt(i)==p.charAt(j) || p.charAt(j)=='.'))
                ans = isMatchHelper(s,p,dp,i+1,j+1); 
+            else 
+                ans = false;
         }
         
         dp[i][j] =  ans;
         return dp[i][j];
     }
+    
+    /*
+    Nice thing about this is we tried to find * in the next position. That made life easier. 
+    */
 }
