4f62cc79ce8013638df85baa72a40ffca4b7997c
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-09-19 19:30:25 -0700

    Update DSG : Twitter.java

--- DSG : Twitter.java
+++ DSG : Twitter.java
@@ -1,8 +1,17 @@
 /*
-Design a simplified version of Twitter where users can post tweets, follow/unfollow another user and is able to see the 10 most recent tweets in the user's news feed. Your design should support the following methods:
+AMZN flipcart msft 3-4
+
+https://leetcode.com/problems/design-twitter/
+
+Design a simplified version of Twitter where users can post tweets, 
+follow/unfollow another user and is able to see the 10 most recent tweets in the user's news feed. 
+Your design should support the following methods:
 
 postTweet(userId, tweetId): Compose a new tweet.
-getNewsFeed(userId): Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+getNewsFeed(userId): Retrieve the 10 most recent tweet ids in the user's news feed. 
+
+Each item in the news feed must be posted by users who the user followed or by the user herself. 
+Tweets must be ordered from most recent to least recent.
 follow(followerId, followeeId): Follower follows a followee.
 unfollow(followerId, followeeId): Follower unfollows a followee.
 */
