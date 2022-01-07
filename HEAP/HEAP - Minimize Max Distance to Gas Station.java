079f7b0ddc34af8ef7f3ccb358ba9278f2f3af3c
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2022-01-06 20:52:26 -0800

    update

--- HEAP/HEAP - Minimize Max Distance to Gas Station.java
+++ HEAP/HEAP - Minimize Max Distance to Gas Station.java
@@ -1,4 +1,7 @@
 /*
+Hard -
+only Google2 in last two years.
+
 774. Minimize Max Distance to Gas Station
 https://leetcode.com/problems/minimize-max-distance-to-gas-station/
 
@@ -20,53 +23,77 @@ Answers within 10^-6 of the true value will be accepted as correct.
 */
 
 class Solution {
-/*    
+/*
     Solution 1: for each K we find max distance between 2 gas station and add station in beetween. (N^2)
     Solution 2: TLE
             1. We put diff on MaxHeap with one station in it.
             2. We pop and we put the one more station in the that distance,
                 so the avg distance becomes distance/stations.
             3. we keep adding K stations.
-    
-    Solution 3: is bonary search too diff so i am not doing this. 
-    
-    
+
+
+
    Time :  O(N*Log(N)+ K*Log(N))
    Space : O(n)
-*/  
+*/
     public class Pair {
         public double distance;
         public double stations;
-        
+
         Pair(double d, double s){
-            distance = d; 
+            distance = d;
             stations = s;
         }
-        
+
         public double avgDistance(){
             return distance/stations;
         }
-        
+
     }
-    
+
     public double minmaxGasDist(int[] stations, int K) {
         PriorityQueue<Pair> maxHeap = new PriorityQueue<Pair>(new Comparator<Pair>(){
             public int compare(Pair a, Pair b){
-                return  a.avgDistance() < b.avgDistance() ? 1 : -1; 
+                return  a.avgDistance() < b.avgDistance() ? 1 : -1;
             }
         });
-        
+
         for(int i=0; i<stations.length-1; i++){
             maxHeap.add(new Pair(stations[i+1] - stations[i], 1));
         }
-        
+
         while(K>0){
             Pair p = maxHeap.poll();
             p.stations++;
             maxHeap.add(p);
             K--;
         }
-        
+
         return maxHeap.poll().avgDistance();
     }
+
+
+    // This is wrong algorithm
+    // Imagine we have to add 2 stattion and the largest distance is 30
+    // we add in the middle so now we have 15 and 15 distance. then we add a station in one of the 15 diff.
+    // we should be adding 2 station at 10 and 20 -- so the min dstance becomes 10
+    public double minmaxGasDist(int[] stations, int k) {
+        if(stations==null || stations.length==0)
+            return 0;
+        PriorityQueue<Double> pq = new PriorityQueue<Double>( Collections.reverseOrder());
+
+        for(int i=0; i<stations.length-1; i++){
+            pq.add(stations[i+1]*1.0 - stations[i]*1.0);
+        }
+
+        for(int i=0;i<k; i++){
+            double dist = pq.poll();
+            System.out.println(dist);
+            dist = dist/2.0;
+            pq.add(dist);
+            pq.add(dist);
+        }
+        return pq.poll();
+    }
+
 }
--- HEAP/HEAP - split array consecutive 3number or more.java
+++ HEAP/HEAP - split array consecutive 3number or more.java
@@ -1,8 +1,8 @@
 /*
 https://leetcode.com/problems/split-array-into-consecutive-subsequences/
 
-Given an integer array nums that is sorted in ascending order, 
-return true if and only if you can split it into one or more subsequences such 
+Given an integer array nums that is sorted in ascending order,
+return true if and only if you can split it into one or more subsequences such
 that each subsequence consists of consecutive integers and has a length of at least 3.
 
 Input: nums = [1,2,3,3,4,5]
@@ -13,7 +13,7 @@ Explanation:
 
 Input: nums = [1,2,3,3,4,4,5,5]
 Output: true
-You can split them into two consecutive subsequences : 
+You can split them into two consecutive subsequences :
 1, 2, 3, 4, 5
 3, 4, 5
 
@@ -27,9 +27,21 @@ The order of 3 cases above matters. For easier implementation, Case 3 should be 
 In the priority queue, all intervals are sorted by end increasingly, if there is a tie, we sort them by size increasingly.
 */
 
-class Solution { 
+class Solution {
+    class Interval {
+        int start;
+        int end;
+        int length;
+
+        public Interval(int start, int end) {
+            this.start = start;
+            this.end = end;
+            length = end - start + 1;
+        }
+    }
+
     public boolean isPossible(int[] nums) {
-        
+
         PriorityQueue<Interval> pq = new PriorityQueue<>(new Comparator<Interval>(){
             @Override
             public int compare(Interval a, Interval b) {
@@ -38,9 +50,9 @@ class Solution {
                 }
                 return Integer.compare(a.end, b.end);
             }
-        }); 
+        });
         // PriorityQueue<Interval> pq = new PriorityQueue<>( (a, b) -> (a.end == b.end ? a.len - b.len: a.end - b.end ) );
-        
+
         for (int num : nums) {
             while (!pq.isEmpty() && pq.peek().end + 1 < num) {
                 if (pq.poll().length < 3)
@@ -52,77 +64,67 @@ class Solution {
                 pq.add(new Interval(pq.poll().start, num));
             }
         }
-        
+
         while (!pq.isEmpty()) {
             if (pq.poll().length < 3)
                 return false;
         }
-        
+
         return true;
     }
-    
-    class Interval {
-        int start;
-        int end;
-        int length;
-        
-        public Interval(int start, int end) {
-            this.start = start;
-            this.end = end;
-            length = end - start + 1;
-        }
-    }
+
+
 }
 
-/*  Using deque and Priority Queue. 
-Another subtle insight to this solution is proving that appending to a previous sequence is always better than creating a new sequence. 
+/*  Using deque and Priority Queue.
+Another subtle insight to this solution is proving that appending to a previous sequence is always better than creating a new sequence.
 */
      public boolean isPossible(int[] nums) {
-        
+
          PriorityQueue<Deque<Integer>> pq = new PriorityQueue<>( new Comparator<Deque<Integer>>(){
             public int compare(Deque<Integer> a, Deque<Integer> b){
                 if(a.peekLast() == b.peekLast()){
-                   return a.size() - b.size(); 
+                   return a.size() - b.size();
                 } else {
                     return a.peekLast() - b.peekLast();
                 }
             }
          });
-        
-        
+
+
          for(int i=0; i<nums.length; i++){
 
             while(!pq.isEmpty() && pq.peek().peekLast() < nums[i]-1){
                 Deque<Integer> deque = pq.poll();
                 if(deque.size()<3)
-                    return false; 
+                    return false;
             }
-            
+
             if(pq.isEmpty()){
                 Deque<Integer> deque = new ArrayDeque<Integer>();
                 deque.add(nums[i]);
                 pq.add(deque);
-                continue; 
+                continue;
             }
-            
+
             else if(pq.peek().peekLast() == nums[i]){
                 Deque<Integer> newDeque = new ArrayDeque<Integer>();
                 newDeque.add(nums[i]);
                 pq.add(newDeque);
             }
-             
+
             else if (pq.peek().peekLast() == nums[i]-1){
                 Deque<Integer> deque = pq.poll();
                 deque.add(nums[i]);
                 pq.add(deque);
             }
-            
+
          }
-         
+
          while(!pq.isEmpty()){
             Deque<Integer> deque = pq.poll();
                if(deque.size()<3)
-                   return false; 
+                   return false;
          }
          return true;
      }
