17b52235842b4ceec659c7bbd34a869dfdd507e3
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2024-02-26 12:56:38 -0800

    Update Backtracking - Generate All Parentheses combinations given n.java

--- Parentheses/Backtracking - Generate All Parentheses combinations given n.java
+++ Parentheses/Backtracking - Generate All Parentheses combinations given n.java
@@ -2,6 +2,8 @@
 Medium - VVIMP
 https://leetcode.com/problems/generate-parentheses/
 Sooo many companie
+
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 For example, given n = 3, a solution set is:
 [
   "((()))",
